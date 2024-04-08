91ef4f1fd835e2db01422a4e1d1c18e6fa02b91c
Lovelyfin00 <omokaroloveth10@gmail.com>
2024-04-08 21:19:11 +0100

    feat:added resources page

---
+++ src/app/resources/assort/page.tsx
@@ -0,0 +1,11 @@
+import Assort from "./assort"
+
+const page = () => {
+  return (
+    <section>
+        <Assort />
+    </section>
+  )
+}
+
+export default page
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-brown-10": "rgb(238, 240, 255)",
+        "light-brown": "#E3E5FF",
+        "brown-800": "rgb(9 50 139)",
+        "brown-600": "rgb(227, 229, 255)",
+
+        "neutral-black": "#09328b",
+
+        "text-blue": "#224ba4",
+
+        "navy-400": "rgb(139 159 200)",
+        "navy-600": "rgb(34 75 164)",
+      }
     },
   },
   plugins: [],
