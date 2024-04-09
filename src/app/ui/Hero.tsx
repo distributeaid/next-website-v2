d22fe3b93ea9383de99e35956085f0429eaab2b1
rica213 <25f5k252l@mozmail.com>
2024-04-09 12:33:04 +0300

    Implement the hero section

---
+++ src/app/ui/Hero.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import {
+  logoHero,
+  loadingTruck,
+  moriaFireRelief,
+  soapRelief,
+  waterRelief,
+} from "@/data/home";
+import DynamicBgImg from "./DynamicBgImg";
+
+const Hero = () => {
+  const setImagesI = [moriaFireRelief.src, soapRelief.src, loadingTruck.src];
+  const setImagesII = [soapRelief.src, loadingTruck.src, waterRelief.src];
+  const setImagesIII = [loadingTruck.src, waterRelief.src, moriaFireRelief.src];
+  return (
+    <section className="flex gap-20 py-32 justify-center flex-nowrap flex-shrink-0">
+      <div className="size-[360px] opacity-50 translate-y-5 flex-none">
+        <DynamicBgImg currentBg={waterRelief.src} otherBgImages={setImagesI} />
+      </div>
+      <div className="flex w-[50%] h-[400px] justify-between items-center flex-shrink-0">
+        <div className="w-[50%] pr-4">
+          <Image src={logoHero} alt="" className="py-2" />
+          <p className="text-primary text-[2.15rem] leading-[3rem] ">Re-imagine {<br/>}Humanitarian Aid {<br/>}Delivery</p>
+        </div>
+        <div className="flex-1 h-full">
+          <DynamicBgImg currentBg={moriaFireRelief.src} otherBgImages={setImagesII} />
+        </div>
+      </div>
+      <div className="size-[360px] opacity-50 -translate-y-10 flex-none">
+        <DynamicBgImg currentBg={soapRelief.src} otherBgImages={setImagesIII} />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
