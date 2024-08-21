d45f7b347d20da9985ff4d13491b0a5f30ae4d29
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-08-21 21:28:24 +0300

    deps: set up testing suite

--- src/components/ui/fonts.ts
+++ src/components/ui/fonts.ts
@@ -1,6 +1,6 @@
-import { Lato } from "next/font/google";
+// import { Lato } from "next/font/google";
 
-export const lato = Lato({
-  weight: ["100", "300", "400", "700", "900"],
-  subsets: ["latin"],
-});
+// export const lato = Lato({
+//   weight: ["100", "300", "400", "700", "900"],
+//   subsets: ["latin"],
+// });
