37c2d43ce2ca456e268d45bc4bf02ffa98786ec2
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2024-09-09 20:00:33 +0300

    fix: add more detailed test example

--- src/tests/HelloWorld.test.tsx
+++
@@ -1,7 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-describe("Testing for the string hello world", () => {
-  it("returns hello world", () => {
-    expect("Hello World").toEqual("Hello World");
-  });
-});
