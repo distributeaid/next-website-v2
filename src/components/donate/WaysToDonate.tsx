c324a77df850a4d866934e576377672243791629
Jim Cummins <jimthedev@gmail.com>
2024-11-11 01:12:38 -0600

    fix: restyle donation buttons, add omprakash

--- src/components/donate/WaysToDonate.tsx
+++ src/components/donate/WaysToDonate.tsx
@@ -6,63 +6,72 @@ import Image from "next/image";
 import siteSettings from "../../data/site-settings.json";
 import Link from "next/link";
 
-const Divider = () => <hr className="w-[110%] h-1 bg-rosemary-300 px-8" />;
-
 export const WaysToDonate = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
     <div className="flex flex-col gap-8 md:gap-16 items-center justify-center md:flex-row bg-rosemary-50">
-      {/* PayPal
-      ------------------------------------------------------------ */}
-      <Link
-        className="flex flex-col gap-2 items-center hover:drop-shadow-md"
-        href={siteSettings.donate.byPaypal}
-      >
-        <Image
-          className="h-9"
-          src="/images/paypal-color.svg"
-          alt="PayPal Logo"
-          width={200}
-          height={200}
-        />
-        <Divider />
-        <Button variant="rosemary">Donate with PayPal</Button>
-      </Link>
+      <DonateWithPayPalLink />
+      <DonateWithDebitCreditCardLink />
+      <DonateWithBankTransferLink />
+    </div>
+  );
+};
 
-      {/* Open Collective
-      ------------------------------------------------------------ */}
-      <Link
-        className="flex flex-col gap-2 items-center hover:drop-shadow-md"
-        href={siteSettings.donate.byOpenCollective}
-      >
-        <Image
-          className="h-9"
-          src="/images/opencollective-wordmark.svg"
-          width={200}
-          height={200}
-          alt="Open Collective Logo"
-        />
-        <Divider />
-        <Button variant="rosemary">Use a Debit / Credit Card</Button>
-      </Link>
+export const DonateWithPayPalLink = () => {
+  return (
+    <Link
+      className="flex flex-col gap-2 items-center hover:drop-shadow-md"
+      href={siteSettings.donate.byPaypal}
+    >
+      <Image
+        className="mt-7 h-9"
+        src="/images/paypal-color.svg"
+        alt="PayPal Logo"
+        width={200}
+        height={36}
+      />
+      <Button variant="default" className="rounded-none">Donate with PayPal</Button>
+    </Link>
+  );
+};
 
-      {/* Bank Transfer
-      ------------------------------------------------------------ */}
-      <div
-        className="h-full flex flex-col gap-2 items-center cursor-pointer hover:drop-shadow-md"
-        onClick={() => setModalIsOpen(true)}
+export const DonateWithDebitCreditCardLink = () => {
+  return (
+    <Link
+      className="flex flex-col gap-2 items-center hover:drop-shadow-md"
+      href={siteSettings.donate.byOmprakash}
+    >
+      <Image
+        className="h-16"
+        src="/images/omprakash.png"
+        width={130}
+        height={86}
+        alt="Omprakash Logo"
+      />
+      <Button variant="primary" className="rounded-none">Use a Debit/Credit card</Button>
+    </Link>
+  );
+};
+
+export const DonateWithBankTransferLink = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  return (
+    <div className="flex flex-col gap-2 items-center">
+      <Image
+        src="/images/direct_transfer.svg"
+        className="md:mt-7 h-9"
+        alt="Dollar sign"
+        width={200}
+        height={200}
+      />
+      <Button
+        variant="primary"
+        className="rounded-none"
+        onClick={() => {
+          setModalIsOpen(true);
+        }}
       >
-        <Image
-          src="/images/direct_transfer.svg"
-          className="h-9 w-9"
-          alt="Dollar sign"
-          width={200}
-          height={200}
-        />
-        <Divider />
-        <Button variant="rosemary">Make a Bank Transfer</Button>
-      </div>
-      {/* NOTE: absolutely positioned, so not in flex flow */}
+        Make a Bank Transfer
+      </Button>
       <BankInformationModal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
