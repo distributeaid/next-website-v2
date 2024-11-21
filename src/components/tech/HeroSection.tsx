1b3d9df75f74c110f7ac2c9fd08fb59e9865790e
Urji  Tahir <utahir2@gmail.com>
2024-11-21 04:16:57 +0000

    Create hero section for Tech page

--- src/components/tech/HeroSection.tsx
+++ src/components/tech/HeroSection.tsx
@@ -5,9 +5,9 @@ import heroBackgroundImg from '../../../public/images/tech/tech-hero-background-
 const HeroSection = () => {
     return (
         <div className='block w-[1252px] item-center justify-center p-8 relative '>
-            <h1 className='text-center text-white uppercase font-bold md:text-5xl md:py-6 py-2 z-20 sm:text-3xl text-xl'>TECH AT DISTRIBUTE AID</h1>
+            <h1 className='text-center text-white uppercase font-bold text-5xl p-2 leading-[52.73px] z-20'>TECH AT DISTRIBUTE AID</h1>
            
-                <Box className="w-full relative flex align-center items-center justify-center mt-4" >
+                <Box className="lg:w-full md:w-1/2 relative flex align-center items-center justify-center mt-4" >
                     
                     <Image
                         src={heroBackgroundImg}
@@ -15,16 +15,16 @@ const HeroSection = () => {
                         className="w-full h-[520px] object-cover rounded-bl-[32px] rounded-br-[32px]"
                         
                     /> 
-                    <Box className='gap-3 bg-[#252525B2]/70 text-white flex flex-col absolute rounded-bl-[32px] rounded-br-[32px] inset-0 justify-center item-center text-center '>
+                    <Box className='gap-2 bg-gray-800/60 w-full text-white flex flex-col absolute rounded-bl-[32px] rounded-br-[32px] inset-0 justify-center item-center text-center'>
                        
-                        <Heading className='text-lg lg:text-5xl md:text-4xl sm:text-3xl md:mt-16 mt-4 font-bold drop-shadow-[0px_4px_6px_rgba(5,30,93,0.7)]'>Open-Source Solutions For Global Aid Delivery</Heading>
-                        <Text className='text-sm md:text-lg sm:text-md font-roboto font-bold leading-[28.13px] md:mx-32 md:mt-10 p-4 text-center drop-shadow-[0px_4px_6px_rgba(5,30,93,0.6)]'>Our diverse tech team of volunteers works hard to create technical solutions that make Distribute Aid’s work more efficient and easier to scale even with the limited resources NGOs have. We provide solutions that can be replicated across the sector by our partners and other NGOs in the field which is one of the reasons why our work is open-sourced.
+                        <Heading className='md:text-[45px] sm:text-[34px] mt-10 font-bold drop-shadow-[0px_4px_6px_rgba(5,30,93,0.7)]'>Open-Source Solutions For Global Aid Delivery</Heading>
+                        <Text className='md:text-[22px] sm:text-md font-roboto font-bold leading-[28.13px] mx-32 mt-10  text-center drop-shadow-[0px_4px_6px_rgba(5,30,93,0.7)]'>Our diverse tech team of volunteers works hard to create technical solutions that make Distribute Aid’s work more efficient and easier to scale even with the limited resources NGOs have. We provide solutions that can be replicated across the sector by our partners and other NGOs in the field which is one of the reasons why our work is open-sourced.
                         <br></br>
                         <br></br>
                         Read on to learn more about our tech team and our Open Source Explorers program.</Text>
-                        <Flex className='flex justify-center gap-5 item-center  '>
-                            <Button className='sm:px-10 sm:py-8 px-4 py-2 text-sm  font-normal'>Join Our Team</Button>
-                            <Button className='sm:px-6 sm:py-8 text-sm  bg-white text-blue-950 font-normal'>Become a Sponsor</Button>
+                        <Flex className='flex justify-center gap-5 item-center mt-10 '>
+                            <Button className='px-10 py-8 text-sm  font-normal'>Join Our Team</Button>
+                            <Button className='px-6 py-8 text-sm  bg-white text-blue-950 font-normal'>Become a Sponsor</Button>
                         </Flex>
                       
                     </Box>
