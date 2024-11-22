ff05fe108328481d1b3f866f97d620fc295be6a0
Urji  Tahir <utahir2@gmail.com>
2024-11-22 00:04:40 +0000

    Fix formatting issues

--- postcss.config.js
+++ postcss.config.js
@@ -6,9 +6,9 @@ module.exports = {
       },
     },
   },
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
