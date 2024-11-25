0c28555e714c28ce98a3a58b74d37de5d2e60d7e
Harshvardhan1012 <hs883532@gmail.com>
2024-11-25 12:10:19 +0530

    aboutus-changes

--- src/components/about-us/AboutOurMissionVision.tsx
+++ src/components/about-us/AboutOurMissionVision.tsx
@@ -1,20 +1,28 @@
+import { Flex, Heading, Text } from "@radix-ui/themes";
 import { FC } from "react";
-import ReactMarkdown from "react-markdown";
 
 type Props = {
   title: string;
-  content: string;
 };
 
-const AboutOurMissionVision: FC<Props> = ({ title, content }) => (
-  <div className="max-w-5xl mx-auto px-0 py-5 md:px-5 md:py-5 lg:py-20  text-lg text-gray-700 flex items-center flex-col h-full">
-    <h2 className="text-2xl font-semibold mb-8 text-gray-800">
-      About our {title}
-    </h2>
-    <ReactMarkdown className="text-lg font-normal leading-[35px] tracking-[-0.16px] text-center text-black">
-      {content}
-    </ReactMarkdown>
-  </div>
-);
+const data = {
+  aboutOurMission: `We are re-imagining humanitarian aid delivery. We aim to help move humanitarian aid to where it is needed most, create an efficient and time-saving system for shipments of aid, and lower carbon emissions for humanitarian aid. Distribute Aid has the end goal of producing a platform for the use of aid collection and service, providing groups that will incorporate all of these aims seamlessly. We offer support to a huge network of grassroots organizations working within the Refugee Aid movement in Europe, and COVID-19 response groups in Europe and the US`,
+  aboutOurVision: `Distribute Aid wants to bring more transparency to all groups involved around what donations are on offer, and make it easier to see what is needed where, which also prevents waste. Creating a platform for aid delivery will connect hundreds of independent groups working in the same field, for the same greater cause but who currently have little oversight. This platform will also provide a way to collect data on regional needs, providing a wider overview of needs and assisting in securing in-kind donations to a scale never before possible!`,
+};
 
-export default AboutOurMissionVision;
+export const AboutOurMissionVision: FC<Props> = ({ title }) => (
+  <Flex
+    px={{ md: "4" }}
+    py={{ md: "4" }}
+    direction="column"
+    align="center"
+    gap="4"
+  >
+    <Heading size="6" weight="bold">
+      About our {title}
+    </Heading>
+    <Text className="leading-[35px]" size="4" align="center" >
+      {title === "Mission" ? data.aboutOurMission : data.aboutOurVision}
+    </Text>
+  </Flex>
+);
--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -6,13 +6,24 @@ type Image = {
   width: number;
   height: number;
   altText: string;
+  caption: string;
+  attribution: string;
   alignment: "left" | "center" | "right";
 };
 
 // const [isHovering, setIsHovering] = useState(false)
 
-const Images: FC<Image> = ({ image, width, height, altText, alignment }) => {
+const Images: FC<Image> = ({
+  image,
+  width,
+  height,
+  altText,
+  caption,
+  attribution,
+  alignment,
+}) => {
   let flexAlignment = getFlexAlignment(alignment);
+  let textAlignment = getTextAlignment(alignment);
 
   return (
     <div className={`flex ${flexAlignment}`}>
@@ -24,6 +35,10 @@ const Images: FC<Image> = ({ image, width, height, altText, alignment }) => {
         <div className="relative">
           <Image src={image} alt={altText} height={height} width={width} />
         </div>
+        <p className={`text-neutral-500 text-sm ${textAlignment}`}>
+          Photo Credit: {attribution}
+        </p>
+        <p className={`${textAlignment}`}>{caption}</p>
       </div>
     </div>
   );
@@ -38,5 +53,15 @@ export const getFlexAlignment = (alignment: string) => {
     return "justify-start";
   } else {
     return "justify-center";
+  }
+};
+
+export const getTextAlignment = (alignment: string) => {
+  if (alignment === "right") {
+    return "text-right";
+  } else if (alignment === "left") {
+    return "text-left";
+  } else {
+    return "text-center";
   }
 };
