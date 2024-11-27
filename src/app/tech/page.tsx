ad72190fb289b3c8c933622f7ad271558832853c
Urji  Tahir <utahir2@gmail.com>
2024-11-27 21:00:32 +0000

    fixed error on changes

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,16 +1,15 @@
 import HeroSection from "@/components/tech/HeroSection";
 import { Box, Flex } from "@radix-ui/themes";
 
-
 const page = () => {
   return (
-    <Flex 
-      direction="column"       
-      justify="start" 
+    <Flex
+      direction="column"
+      justify="start"
       align="center"
-      className="relative w-full" 
+      className="relative w-full"
       gap={{ initial: "4", lg: "6", xl: "9" }}
-      >
+    >
       <Box className="absolute inset-0 bg-navy-900 w-full h-[450px] z-0" />
       <HeroSection />
     </Flex>
