67af78c693646b444f998a59ae5977b99564cadb
Claire Lin <chunhuilin7516@gmail.com>
2024-11-28 20:22:49 +0100

    Fix formatting

--- src/app/donate/new-donate/page.tsx
+++ src/app/donate/new-donate/page.tsx
@@ -4,18 +4,20 @@ import { FUND_RAISERS } from "@/data/donate";
 const Page = () => {
   return (
     <div>
-      {FUND_RAISERS.map((donate, i) =>
+      {FUND_RAISERS.map((donate, i) => (
         <CampaignCard
           key={i}
-          imgSrc={donate.img || "/images/donate/european-refugee-relief-hero.webp"}
+          imgSrc={
+            donate.img || "/images/donate/european-refugee-relief-hero.webp"
+          }
           imgAlt="Alt"
-          title={donate.title || "Untitled Campaign" }
+          title={donate.title || "Untitled Campaign"}
           raised={donate.percentage ?? 0}
           goal={donate.goal_price ?? 0}
           donateLink={donate.donate_link || "#"}
           moreLink={donate.more_link || "#"}
         />
-      )}
+      ))}
     </div>
   );
 };
