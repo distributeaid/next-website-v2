38bc88e83d3eb3d47eb464bc3c1acd9d7f4db896
Harshvardhan1012 <hs883532@gmail.com>
2024-12-02 09:52:24 +0530

    fix-aboutuschanges

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,32 +1,21 @@
 import React, { FC } from "react";
-import AboutHero from "@/components/about-us/AboutHero";
-import { AboutOurMissionVision } from "@/components/about-us/AboutOurMissionVision";
-import { TimelineItem } from "@/components/about-us/Timeline";
+import { AboutHero } from "@/components/about-us/AboutHero";
+import { Timeline } from "@/components/about-us/Timeline";
 import { CallOutText } from "@/components/about-us/Callout";
-import { Flex, Separator } from "@radix-ui/themes";
+import { SideBySide } from "@/components/text/SideBySide";
 
 const AboutUsPage: FC = () => {
+  const showCallOut = false; //toggle to display callout
   return (
     <main>
-      <CallOutText message="some message here" color="green" />
+      {showCallOut && <CallOutText message="some message here" color="green" />}
       <AboutHero />
-      <Flex
-        py="9"
-        direction={{ md: "row", initial: "column" }}
-        justify="center"
-        align="center"
-        gap={{ initial: "4", lg: "6", xl: "9" }}
-        px={{ initial: "4", md: "9" }}
-        className="h-full bg-gray-50"
-      >
-        <AboutOurMissionVision title="Mission" />
-        <Separator
-          orientation="vertical"
-          className="hidden lg:block self-stretch h-auto"
-        />
-        <AboutOurMissionVision title="Vision" />
-      </Flex>
-      <TimelineItem />
+      <SideBySide
+        missionTitle="Mission"
+        visionTitle="Vision"
+        bgColor="gray-50"
+      />
+      <Timeline />
     </main>
   );
 };
