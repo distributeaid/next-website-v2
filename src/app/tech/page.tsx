df12597433fb13ac84ed6fdf4eccd2fcdec0bce5
Taylor Fairbank <taylor@distributeaid.org>
2024-12-02 02:50:38 +0000

    refactor: convert tailwind styles to radix

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -4,18 +4,19 @@ import { Box, Flex } from "@radix-ui/themes";
 const page = () => {
   return (
     <Flex
+      position="relative"
+      width="100%"
       direction="column"
-      justify="start"
       align="center"
-      className="relative w-full"
+      justify="start"
       gap={{ initial: "4", lg: "6", xl: "9" }}
     >
       <Box
-        width="full"
+        width="100%"
         position="absolute"
         inset="0"
         height="450px"
-        className="bg-navy-900 z-0"
+        className="bg-navy-900 -z-10"
       />
 
       <HeroSection />
--- src/components/tech/HeroSection.tsx
+++ src/components/tech/HeroSection.tsx
@@ -1,80 +1,101 @@
-import { Flex, Heading, Text, Button, Box } from "@radix-ui/themes";
+import { Container, Flex, Box, Heading, Text, Link, Button } from "@radix-ui/themes";
 
 const HeroSection = () => {
   return (
-    <Flex
-      direction="column"
-      justify="center"
-      className="w-full items-center px-20"
-    >
-      <Box
-        width="full"
-        py="2"
-        className="text-center text-white uppercase font-bold md:py-8 z-20"
-      >
-        <Heading as="h1" size={{ initial: "5", sm: "7", md: "9" }}>
-          TECH AT DISTRIBUTE AID
-        </Heading>
-      </Box>
+    <Container size={{xs: "1", sm: "2", md: "3", lg: "4"}} align="center">
       <Flex
+        width="100%"
         direction="column"
         align="center"
         justify="center"
-        className="w-full h-[520px] relative rounded-bl-[32px] rounded-br-[32px] bg-cover bg-center bg-[url('/images/tech/tech-hero-background-img.jpg')]"
       >
-        <Flex
-          direction="column"
-          gap="3"
-          justify="center"
-          align="center"
-          className="h-full bg-[#252525B2]/60 text-white rounded-bl-[32px] rounded-br-[32px] inset-0 text-center "
+        <Box
+          pt={{initial: "1", md: "4"}}
+          pb={{initial: "2", md: "6"}}
+          asChild={true}
         >
           <Heading
-            as="h4"
-            size="5"
-            className="text-center md:text-5xl md:mt-10 mt-4 font-bold drop-shadow-[0px_4px_6px_rgba(5,30,93,0.7)]"
+            as="h1"
+            size={{ initial: "7", sm: "8", md: "9" }}
+            weight="bold"
+            align="center"
+            className="uppercase text-white"
           >
-            Open-Source Solutions For Global Aid Delivery
+            TECH AT DISTRIBUTE AID
           </Heading>
-          <Text
-            size={{ initial: "2", md: "6" }}
-            weight="bold"
-            align="center"
-            className="font-roboto md:mx-32 md:mt-8 p-2 drop-shadow-[0px_4px_6px_rgba(5,30,93,0.6)]"
-          >
-            Our diverse tech team of volunteers works hard to create technical
-            solutions that make Distribute Aid’s work more efficient and easier
-            to scale even with the limited resources NGOs have. We provide
-            solutions that can be replicated across the sector by our partners
-            and other NGOs in the field which is one of the reasons why our work
-            is open-sourced.{" "}
-          </Text>
-
-          <Text
-            size={{ initial: "2", md: "6" }}
-            weight="bold"
-            align="center"
-            className="font-roboto md:mx-32 p-4 drop-shadow-[0px_4px_6px_rgba(5,30,93,0.6)]"
-          >
-            Read on to learn more about our tech team and our Open Source
-            Explorers program.
-          </Text>
-
+        </Box>
+        <Flex
+          position="relative"
+          width="100%"
+          direction="column"
+          align="center"
+          justify="center"
+          className="rounded-bl-[32px] rounded-br-[32px] bg-cover bg-center bg-[url('/images/tech/tech-hero-background-img.jpg')]"
+        >
           <Flex
-            direction={{ initial: "column", md: "row" }}
-            justify={{ initial: "start", md: "center" }}
+            direction="column"
+            gap="4"
+            justify="center"
             align="center"
-            gap={{ initial: "1", md: "5" }}
-            className="w-full py-4"
+            px={{initial:"4", sm: "6"}}
+            py={{initial:"6", sm: "8"}}
+            className="text-white bg-[#252525B2]/60 rounded-bl-[32px] rounded-br-[32px] inset-0 text-center "
           >
-            <Button size="4">Join Our Team</Button>
-            <Button size="4" className="bg-white text-blue-950 font-normal">
-              Become a Sponsor
-            </Button>
+            <Heading
+              as="h2"
+              size={{ initial: "6", sm: "7", md: "8" }}
+              weight="bold"
+              align="center"
+              mb={{ initial: "2", sm: "4" }}
+              className="drop-shadow-[0px_4px_6px_rgba(5,30,93,0.7)]"
+            >
+              Open-Source Solutions For Global Aid Delivery
+            </Heading>
+            <Text
+              size={{ initial: "3", sm:"4" }}
+              weight="bold"
+              align="center"
+              className="font-roboto drop-shadow-[0px_4px_6px_rgba(5,30,93,0.6)]"
+            >
+              Our diverse tech team of volunteers works hard to create technical
+              solutions that make Distribute Aid’s work more efficient and easier
+              to scale even with the limited resources NGOs have. We provide
+              solutions that can be replicated across the sector by our partners
+              and other NGOs in the field which is one of the reasons why our work
+              is open-sourced.
+            </Text>
+
+            <Text
+              size={{ initial: "3", sm:"4" }}
+              weight="bold"
+              align="center"
+              className="font-roboto drop-shadow-[0px_4px_6px_rgba(5,30,93,0.6)]"
+            >
+              Read on to learn more about our tech team and our Open Source
+              Explorers program.
+            </Text>
+
+            <Flex
+              width="100%"
+              direction={{ initial: "column", sm: "row" }}
+              justify={{ initial: "start", sm: "center" }}
+              align="center"
+              gap={{ initial: "3", sm: "5" }}
+              mt={{ initial: "2", sm: "4" }}
+            >
+              <Link href="mailto:tech@distributeaid.org">
+                <Button size="4">Join Our Team</Button>
+              </Link>
+              <Link href="mailto:tech@distributeaid.org">
+                <Button size="4" className="bg-white text-blue-950 font-normal">
+                  Become A Sponsor
+                </Button>
+              </Link>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
-    </Flex>
+    </Container>
   );
 };
 
