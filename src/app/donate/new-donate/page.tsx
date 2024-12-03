8721780470880c28bd14e09662977779681a8c70
Heather Cavanaugh <hcavanaugh24@gmail.com>
2024-12-03 23:03:55 +0000

    Donate Page grid WIP, await Component: Donate campaign Cards #91

--- src/app/donate/new-donate/page.tsx
+++ src/app/donate/new-donate/page.tsx
@@ -1,22 +1,27 @@
 import CampaignCard from "@/components/donate/CampaignCard";
+import { Container, Grid } from "@radix-ui/themes";
 import { FUND_RAISERS } from "@/data/donate";
 
 const Page = () => {
   return (
-    <div>
-      {FUND_RAISERS.map((donate, i) => (
-        <CampaignCard
-          key={i}
-          imgSrc={donate.img}
-          imgAlt=""
-          title={donate.title}
-          raised={donate.percentage}
-          goal={donate.goal_price}
-          donateLink={donate.donate_link}
-          moreLink={donate.more_link}
-        />
-      ))}
-    </div>
+    <Container px="4">
+      <Grid gap="8" columns={{ xs: "2", md: "3" }} pt="4.5rem" pb="9rem">
+        {FUND_RAISERS && FUND_RAISERS.length > 0 && (
+          FUND_RAISERS.map((donate, i) => (
+            <CampaignCard
+              key={i}
+              imgSrc={donate.img}
+              imgAlt=""
+              title={donate.title}
+              raised={donate.percentage}
+              goal={donate.goal_price}
+              donateLink={donate.donate_link}
+              moreLink={donate.more_link}
+            />
+          ))
+        )}
+      </Grid>
+    </Container>
   );
 };
 
