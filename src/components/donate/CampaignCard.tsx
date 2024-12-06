55db0352c9c739a85fe8db18f84f0e00a9a0b1a3
Taylor Fairbank <taylor@distributeaid.org>
2024-12-06 00:16:10 +0000

    feat: center donate cards on smaller screen sizes

--- src/components/donate/CampaignCard.tsx
+++ src/components/donate/CampaignCard.tsx
@@ -9,10 +9,10 @@ import {
   Progress,
   Text,
 } from "@radix-ui/themes";
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 import Image from "next/image";
 
-const CampaignCard: FC<{
+type Props = {
   imgSrc: string | undefined;
   imgAlt: string | undefined;
   title: string | undefined;
@@ -20,90 +20,103 @@ const CampaignCard: FC<{
   goal: number | undefined;
   donateLink: string | undefined;
   moreLink: string | undefined;
-}> = ({
-  imgSrc = "/images/donate/european-refugee-relief-hero.webp",
-  imgAlt = "",
-  title = "Untitled Campaign",
-  raised = 0,
-  goal = 0,
-  donateLink = "#",
-  moreLink = "#",
-}) => {
-  return (
-    <Box asChild maxWidth="373px">
-      <Card>
-        <Flex direction="column" justify={"between"} height={"100%"}>
-          <Inset clip="padding-box" pb="current">
-            <Image
-              src={imgSrc}
-              alt={imgAlt}
-              height={0}
-              width={0}
-              sizes={"100vw"}
-              style={{
-                display: "block",
-                objectFit: "cover",
-                width: "100%",
-                height: 150,
-                backgroundColor: "var(--blue-9)",
-              }}
-            />
-            <Flex
-              direction="row"
-              align="center"
-              gapX="3"
-              px="4"
-              py="3"
-              style={{
-                backgroundColor: "var(--green-9)",
-              }}
-            >
-              <Progress
-                value={raised}
-                variant="surface"
+  [key: string]: any;
+};
+
+// enable RadixUI's asChild functionality for callig code
+// https://www.radix-ui.com/primitives/docs/guides/composition#composing-with-your-own-react-components
+const CampaignCard: FC<Props> = forwardRef<HTMLDivElement, Props>(
+  (
+    {
+      imgSrc = "/images/donate/european-refugee-relief-hero.webp",
+      imgAlt = "",
+      title = "Untitled Campaign",
+      raised = 0,
+      goal = 0,
+      donateLink = "#",
+      moreLink = "#",
+      ...props
+    },
+    forwardedRef,
+  ) => {
+    return (
+      <Box asChild maxWidth="373px" ref={forwardedRef} {...props}>
+        <Card>
+          <Flex direction="column" justify={"between"} height={"100%"}>
+            <Inset clip="padding-box" pb="current">
+              <Image
+                src={imgSrc}
+                alt={imgAlt}
+                height={0}
+                width={0}
+                sizes={"100vw"}
                 style={{
-                  backgroundColor: "var(--green-4)",
+                  display: "block",
+                  objectFit: "cover",
+                  width: "100%",
+                  height: 150,
+                  backgroundColor: "var(--blue-9)",
                 }}
               />
-              <Text size="2" color="blue" highContrast>
-                {raised}%
-              </Text>
+              <Flex
+                direction="row"
+                align="center"
+                gapX="3"
+                px="4"
+                py="3"
+                style={{
+                  backgroundColor: "var(--green-9)",
+                }}
+              >
+                <Progress
+                  value={raised}
+                  variant="surface"
+                  style={{
+                    backgroundColor: "var(--green-4)",
+                  }}
+                />
+                <Text size="2" color="blue" highContrast>
+                  {raised}%
+                </Text>
+              </Flex>
+            </Inset>
+            <Box mx="1" mt="3">
+              <Heading
+                as="h3"
+                size="8"
+                weight="regular"
+                color="blue"
+                highContrast
+              >
+                {title}
+              </Heading>
+              <Box asChild mb="4">
+                <Text size="3">Goal: €{goal}</Text>
+              </Box>
+            </Box>
+            <Flex gap="2" wrap={"wrap"} mx="1" mb="3" mt="auto">
+              <Box px="4" py="2" asChild>
+                <Button asChild>
+                  <Link href={donateLink} size="3">
+                    Donate now
+                  </Link>
+                </Button>
+              </Box>
+              <Box px="4" py="2" asChild>
+                <Button variant="soft" asChild>
+                  <Link href={moreLink} size="3" className="no-underline">
+                    More info
+                  </Link>
+                </Button>
+              </Box>
             </Flex>
-          </Inset>
-          <Box mx="1" mt="3">
-            <Heading
-              as="h3"
-              size="8"
-              weight="regular"
-              color="blue"
-              highContrast
-            >
-              {title}
-            </Heading>
-            <Box asChild mb="4">
-              <Text size="3">Goal: €{goal}</Text>
-            </Box>
-          </Box>
-          <Flex gap="2" wrap={"wrap"} mx="1" mb="3" mt="auto">
-            <Box px="4" py="2" asChild>
-              <Button asChild>
-                <Link href={donateLink} size="3">
-                  Donate now
-                </Link>
-              </Button>
-            </Box>
-            <Box px="4" py="2" asChild>
-              <Button variant="soft" asChild>
-                <Link href={moreLink} size="3" className="no-underline">
-                  More info
-                </Link>
-              </Button>
-            </Box>
           </Flex>
-        </Flex>
-      </Card>
-    </Box>
-  );
-};
+        </Card>
+      </Box>
+    );
+  },
+);
+
+CampaignCard.displayName = "CampaignCard";
 
 export default CampaignCard;
--- src/components/donate/DonateGrid.tsx
+++ src/components/donate/DonateGrid.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import CampaignCard from "@/components/donate/CampaignCard";
-import { Grid, Container } from "@radix-ui/themes";
+import { Grid, Box, Container } from "@radix-ui/themes";
 import { Fund } from "@/types/fund";
 
 const DonateGrid: FC<{
@@ -10,16 +10,17 @@ const DonateGrid: FC<{
     <Container px="4">
       <Grid gap="8" columns={{ sm: "2", md: "3" }} pt="4.5rem" pb="9rem">
         {funds.map((fund, i) => (
-          <CampaignCard
-            key={i}
-            imgSrc={fund.img}
-            imgAlt=""
-            title={fund.title}
-            raised={fund.percentage}
-            goal={fund.goal_price}
-            donateLink={fund.donate_link}
-            moreLink={fund.more_link}
-          />
+          <Box key={i} width="100%" mx="auto" asChild>
+            <CampaignCard
+              imgSrc={fund.img}
+              imgAlt=""
+              title={fund.title}
+              raised={fund.percentage}
+              goal={fund.goal_price}
+              donateLink={fund.donate_link}
+              moreLink={fund.more_link}
+            />
+          </Box>
         ))}
       </Grid>
     </Container>
