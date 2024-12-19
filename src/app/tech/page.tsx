593d345944722a652d53086fcd462ea9d3d3928a
Michael Edquilan <michaeledquilan@gmail.com>
2024-12-19 18:52:52 +0000

    feat: created sponsor DA section on tech pg

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,26 +1,30 @@
 import HeroSection from "@/components/tech/HeroSection";
+import SponsorSection from "@/components/tech/SponsorSection";
 import { Box, Flex } from "@radix-ui/themes";
 
 const page = () => {
   return (
-    <Flex
-      position="relative"
-      width="100%"
-      direction="column"
-      align="center"
-      justify="start"
-      gap={{ initial: "4", lg: "6", xl: "9" }}
-    >
-      <Box
+    <>
+      <Flex
+        position="relative"
         width="100%"
-        position="absolute"
-        inset="0"
-        height="450px"
-        className="bg-navy-900 -z-10"
-      />
+        direction="column"
+        align="center"
+        justify="start"
+        gap={{ initial: "4", lg: "6", xl: "9" }}
+      >
+        <Box
+          width="100%"
+          position="absolute"
+          inset="0"
+          height="450px"
+          className="bg-navy-900 -z-10"
+        />
 
-      <HeroSection />
-    </Flex>
+        <HeroSection />
+        <SponsorSection />
+      </Flex>
+    </>
   );
 };
 
