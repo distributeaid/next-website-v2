3c8451f87eb9f067a28507e337b879fd7071b2b0
Heather Cavanaugh <hcavanaugh24@gmail.com>
2025-04-03 05:30:30 +0000

    prettify

--- src/components/donate/fund/DonationCTA.tsx
+++ src/components/donate/fund/DonationCTA.tsx
@@ -1,4 +1,12 @@
-import { Box, Container, Heading, Flex, Button, Link, Grid } from "@radix-ui/themes";
+import {
+  Box,
+  Container,
+  Heading,
+  Flex,
+  Button,
+  Link,
+  Grid,
+} from "@radix-ui/themes";
 import Image from "next/image";
 import siteSettings from "../../../data/site-settings.json";
 
@@ -20,78 +28,69 @@ const DonationCTA = () => {
           Ship aid to people in need.
         </Heading>
         <Grid
-            columns={{ initial: "1", sm: "3"}}
-            align="baseline"
+          columns={{ initial: "1", sm: "3" }}
+          align="baseline"
           gap={{ initial: "3", sm: "5" }}
         >
-            <Flex
-                direction="column"
-                gap="3"
-                align="center">
-                <Image
-                    src="/images/donate/paypal.svg"
-                    alt={""}
-                    height={0}
-                    width={0}
-                    sizes={"100vw"}
-                    className="mx-auto max-w-[122px] md:max-w-[150px] w-full"
-                />
-                <Button
-                    className="bg-white text-black group hover:bg-[#4362A6] hover:text-white transition-200 cursor-pointer"
-                    size={{ initial: "2", sm: "3" }}
-                    asChild
-                >
-                    <Link
-                    href={siteSettings.donate.byPaypal}
-                    target="_blank"
-                    underline="none"
-                    >
-                    Donate with Paypal
-                    </Link>
-                </Button>
-            </Flex>
-            <Flex
-                direction="column"
-                gap="3"
-                align="center">
-                <Image
-                    src="/images/donate/omprakash.png"
-                    alt={""}
-                    height={0}
-                    width={96}
-                    sizes={"100vw"}
-                    className="mx-auto"
-                />
-                <Button
-                    className="cursor-pointer"
-                    size={{ initial: "2", sm: "3" }}
-                    asChild
-                >
-                    <Link
-                    href={siteSettings.donate.byOmprakash}
-                    target="_blank"
-                    underline="none"
-                    >
-                    Use a Debit/Credit card
-                    </Link>
-                </Button>
-            </Flex>
-            <Flex
-                direction="column"
-                gap="3"
-                align="center">
-                <Image
-                    src="/images/donate/dollar.svg"
-                    alt={""}
-                    height={0}
-                    width={52}
-                    sizes={"100vw"}
-                    className="mx-auto"
-                />
-                <Button className="cursor-pointer" size={{ initial: "2", sm: "3" }}>
-                    Make a Bank Transfer
-                </Button>
-            </Flex>
+          <Flex direction="column" gap="3" align="center">
+            <Image
+              src="/images/donate/paypal.svg"
+              alt={""}
+              height={0}
+              width={0}
+              sizes={"100vw"}
+              className="mx-auto max-w-[122px] md:max-w-[150px] w-full"
+            />
+            <Button
+              className="bg-white text-black group hover:bg-[#4362A6] hover:text-white transition-200 cursor-pointer"
+              size={{ initial: "2", sm: "3" }}
+              asChild
+            >
+              <Link
+                href={siteSettings.donate.byPaypal}
+                target="_blank"
+                underline="none"
+              >
+                Donate with Paypal
+              </Link>
+            </Button>
+          </Flex>
+          <Flex direction="column" gap="3" align="center">
+            <Image
+              src="/images/donate/omprakash.png"
+              alt={""}
+              height={0}
+              width={96}
+              sizes={"100vw"}
+              className="mx-auto"
+            />
+            <Button
+              className="cursor-pointer"
+              size={{ initial: "2", sm: "3" }}
+              asChild
+            >
+              <Link
+                href={siteSettings.donate.byOmprakash}
+                target="_blank"
+                underline="none"
+              >
+                Use a Debit/Credit card
+              </Link>
+            </Button>
+          </Flex>
+          <Flex direction="column" gap="3" align="center">
+            <Image
+              src="/images/donate/dollar.svg"
+              alt={""}
+              height={0}
+              width={52}
+              sizes={"100vw"}
+              className="mx-auto"
+            />
+            <Button className="cursor-pointer" size={{ initial: "2", sm: "3" }}>
+              Make a Bank Transfer
+            </Button>
+          </Flex>
         </Grid>
       </Container>
     </Box>
