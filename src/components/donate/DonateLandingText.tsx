d75eef6eef190674407a7b4fcf30719c73efcee2
Michael Edquilan <michaeledquilan@gmail.com>
2025-04-10 17:34:56 +0000

    fix: replaced hex colors with radix or tailwind colors and replaced images with emojis

--- src/components/donate/DonateLandingText.tsx
+++ src/components/donate/DonateLandingText.tsx
@@ -1,7 +1,5 @@
 import { Container, Heading, Text, Flex, Link, Strong } from "@radix-ui/themes";
 
-import Image from "next/image";
-
 const DonateLandingText = () => {
   return (
     <Container size={{ sm: "3", lg: "4" }} px={"9"} pb={"9"}>
@@ -9,7 +7,8 @@ const DonateLandingText = () => {
         as="h1"
         weight={"medium"}
         size={{ initial: "8", sm: "9" }}
-        style={{ color: "var(--blue-header)", textAlign: "center" }}
+        style={{ color: "var(--blue-header)" }}
+        align={"center"}
       >
         Donate Supplies
       </Heading>
@@ -30,17 +29,12 @@ const DonateLandingText = () => {
           direction={{ initial: "column", sm: "row" }}
           align={"center"}
         >
-          <Image
-            width={40}
-            height={40}
-            src="/images/donate/uk-flag.png"
-            alt="uk flag"
-          />
+          <Text size={"8"}>🇬🇧</Text>
           <Text align={{ initial: "center", sm: "left" }}>
             <Strong>U.K. -</Strong> email us at 
             <Link
               underline="always"
-              style={{ color: "blue" }}
+              color="blue"
               href="mailto:huib@distributeaid.org"
             >
               hubs@distributeaid.org 
@@ -52,18 +46,13 @@ const DonateLandingText = () => {
           direction={{ initial: "column", sm: "row" }}
           align={"center"}
         >
-          <Image
-            width={40}
-            height={40}
-            src="/images/donate/globe-showing-americas.png"
-            alt="globe showing the americas"
-          />
+          <Text size={"8"}>🌎</Text>
           <Text align={{ initial: "center", sm: "left" }}>
             <Strong>Based elsewhere -</Strong> If you don’t know of any
             collection groups local to you, email us at{" "}
             <Link
               underline="always"
-              style={{ color: "blue" }}
+              color="blue"
               href="mailto:hello@distributeaid.org"
             >
               hello@distributeaid.org
@@ -77,18 +66,13 @@ const DonateLandingText = () => {
           direction={{ initial: "column", sm: "row" }}
           align={"center"}
         >
-          <Image
-            width={40}
-            height={40}
-            src="/images/donate/delivery-truck.png"
-            alt="delivery truck"
-          />
+          <Text size={"8"}>🚚</Text>
           <Text align={{ initial: "center", sm: "left" }}>
             <Strong>Organise a shipment? -</Strong> If you’re coordinating a
             bulk donation and need logistics support, email us at{" "}
             <Link
               underline="always"
-              style={{ color: "blue" }}
+              color="blue"
               href="mailto:logistics@distributeaid.org"
             >
               logistics@distributeaid.org
