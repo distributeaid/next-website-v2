46a7f7edbbca05dd655e47f9d1d411ba208a8a0f
Claire Lin <chunhuilin7516@gmail.com>
2025-06-04 11:49:55 +0200

    refactor(AboutHero): replace Radix layout with Tailwind and adjust blue background positioning for responsive images

--- src/components/about-us/AboutHero.tsx
+++ src/components/about-us/AboutHero.tsx
@@ -4,14 +4,9 @@ import { Box, Flex, Container, Section, Heading, Text } from "@radix-ui/themes";
 
 export const AboutHero: FC = () => (
   <>
-    <Section
-      position="relative"
-      width="100%"
-      pb="164px"
-      mb="100px"
-      className="bg-navy-900"
-    >
-      <Container size="3">
+    <Section className="bg-navy-900 pb-20 relative z-0 sm:pb-12 pt-0 sm:pt-8 md:py-16">
+      {/* heading and text */}
+      <div className="max-w-7xl mx-auto px-8 pb-4 sm:pb-32 sm:px-16 md:px-24 pt-4">
         <Heading
           as="h1"
           weight="bold"
@@ -33,51 +28,51 @@ export const AboutHero: FC = () => (
           We provide for basic human needs at scale by connecting communities
           and empowering people to uphold human dignity.
         </Text>
-      </Container>
-      <Flex
-        position="absolute"
-        bottom="-100px"
-        width="100%"
-        align="start"
-        justify="center"
-        wrap="wrap"
-        gap="5"
-        px="4"
-      >
-        <Image
-          src={"/images/about-us/hero-1.png"}
-          alt="Hero Image"
-          width={200}
-          height={200}
-        />
-        <Image
-          src={"/images/about-us/hero-2.png"}
-          alt="Hero Image"
-          width={200}
-          height={200}
-          className="hidden sm:block"
-        />
-        <Image
-          src={"/images/about-us/hero-3.png"}
-          alt="Hero Image"
-          width={200}
-          height={200}
-          className="hidden md:block "
-        />
-      </Flex>
+      </div>
     </Section>
+    <div className="-mt-16 sm:-mt-32 sm:px-16 z-10 relative w-full flex flex-wrap justify-center gap-2 sm:gap-3 md:gap-5 px-8">
+      <div className="flex-1 max-w-[320px]">
+        <Image
+          src="/images/about-us/hero-1.png"
+          alt="Hero 1"
+          width={0}
+          height={0}
+          sizes="(max-widh: 1024px) 30vw, 200px"
+          className="w-full h-auto rounded-xl"
+        />
+      </div>
+      <div className="flex-1 max-w-[320px]">
+        <Image
+          src="/images/about-us/hero-2.png"
+          alt="Hero 1"
+          width={0}
+          height={0}
+          sizes="(max-widh: 1024px) 30vw, 200px"
+          className="w-full h-auto rounded-xl"
+        />
+      </div>
+      <div className="flex-1 max-w-[320px]">
+        <Image
+          src="/images/about-us/hero-3.png"
+          alt="Hero 1"
+          width={0}
+          height={0}
+          sizes="(max-widh: 1024px) 30vw, 200px"
+          className="w-full h-auto rounded-xl"
+        />
+      </div>
+    </div>
     <Section>
-      <Container size="3">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
         <Heading
-          as="h2"
-          size="9"
+          as="h1"
           weight="bold"
           align="center"
-          className="text-navy-900"
+          className="text-navy-900 text-[32px] sm:text-[48px] md:text-[60px] leading-tight"
         >
           Support us today, so communities are prepared for tomorrow.
         </Heading>
-      </Container>
+      </div>
     </Section>
   </>
 );
