58dc6ba69c6bd85d5c7e6cac2915322d7734a72b
Heather Cavanaugh <hcavanaugh24@gmail.com>
2025-06-05 05:18:37 +0000

    Refactor resources assort page, add new bottom section content

--- src/app/resources/assort/page.tsx
+++ src/app/resources/assort/page.tsx
@@ -1,128 +1,148 @@
-import { Flex, Heading, Separator, Text } from "@radix-ui/themes";
+import { Container, Flex, Separator, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import { PageTitle } from "@/components/resources/assort/PageTitle";
 import { AboutAssort } from "@/components/resources/assort/AboutAssort";
 import { SortingControl } from "@/components/resources/assort/SortingControl";
 import { Guide } from "@/components/resources/assort/Guide";
-import { ResourceRedirect } from "@/components/resources/assort/ResourceRedirect";
 import { ResourcesFooter } from "@/components/resources/assort/ResourcesFooter";
+import SmartLink from "@/components/link/SmartLink";
 
-const data = [
+interface ColumnData {
+  title: string;
+  description: string;
+  src: string;
+}
+
+const columns: Array<ColumnData> = [
   {
     title: "Quality Control",
     description:
       "The receiver may have to pay to dispose of items that are culturally inappropriate, out of season, or poor in quality.",
-    src: "/images/resources/Vector.png",
+    src: "/images/resources/resources_qualitycontrol.png",
   },
   {
     title: "Sorting",
     description:
       "100,000 aid items on a shipment might be sorted one way by the sender and then re-sorted another way by the receiver. Sorting takes time.",
-    src: "/images/resources/Vector (1).png",
+    src: "/images/resources/resources_sorting.png",
   },
 ];
 
 const page = () => {
   return (
-    <section>
+    <>
       <PageTitle title="RESOURCES" bgColor="navy-900" textColor="white" />
-      <AboutAssort color="navy-900" />
-      <Flex
-        gap={{ md: "124px", initial: "6" }}
-        mx={{ md: "9", initial: "3" }}
-        align="center"
-        direction={{ md: "row", initial: "column" }}
-        justify="between"
-      >
-        <SortingControl
-          title={data[0].title}
-          description={data[0].description}
-          src="/images/resources/resources_qualitycontrol.png"
-        />
+      <Container px="4">
+        <AboutAssort color="navy-900" />
+        <Flex
+          gap={{ md: "124px", initial: "6" }}
+          align="center"
+          direction={{ md: "row", initial: "column" }}
+          justify="between"
+        >
+          {columns &&
+            columns.map((col, index) => {
+              return (
+                <>
+                  <SortingControl
+                    title={col.title}
+                    description={col.description}
+                    src={col.src}
+                  />
+                  {columns.length > 0 && index === 0 && (
+                    <Separator
+                      orientation="vertical"
+                      className="h-auto self-stretch hidden md:block"
+                    />
+                  )}
+                </>
+              );
+            })}
+        </Flex>
         <Separator
-          orientation="vertical"
-          className="h-auto self-stretch hidden md:block"
+          orientation="horizontal"
+          className="w-auto mx-16 my-3 lg:block hidden"
         />
-        <SortingControl
-          title={data[1].title}
-          description={data[1].description}
-          src="/images/resources/resources_sorting.png"
-        />
-      </Flex>
-      <Separator
-        orientation="horizontal"
-        className="w-auto mx-16 my-3 lg:block hidden"
-      />
-      <Flex
-        mx={{ md: "9", initial: "3" }}
-        gap="3"
-        className="text-navy-800"
-        direction="column"
-        align="center"
-        my="8"
-      >
-        <Text size="5" align="center">
-          ASSORT leads to faster, cheaper, and more responsive aid. By providing
-          a “common language” for aid donations, organisations receiving aid can
-          know ahead of time what is on an aid shipment.
+        <Flex
+          gap="3"
+          className="text-navy-800"
+          direction="column"
+          align="center"
+          my="8"
+        >
+          <Text size="5" align="center">
+            ASSORT leads to faster, cheaper, and more responsive aid. By
+            providing a “common language” for aid donations, organisations
+            receiving aid can know ahead of time what is on an aid shipment.
+          </Text>
+          <Image
+            src="/images/resources/arrow-down.png"
+            alt="ASSORT"
+            width={22}
+            height={38}
+          />
+          <Text size="4" align="center">
+            When the shipment arrives, they process it much faster than they
+            otherwise could. Indeed, a pilot shipment showed that using ASSORT
+            standards significantly cut down on the receiving organisation’s
+            sorting and processing time. Saving time and volunteer capacity
+            makes a big difference for grassroots aid organisations.
+          </Text>
+        </Flex>
+        <Flex
+          gap={{ initial: "4", md: "0" }}
+          className="text-navy-800"
+          direction={{ md: "row", initial: "column" }}
+          align="center"
+          my="8"
+        >
+          <Guide />
+          <Image
+            src="/images/resources/resources_guide.png"
+            alt="ASSORT"
+            width={548}
+            height={282}
+          />
+        </Flex>
+        <Text
+          align={{ initial: "center", lg: "left" }}
+          size={{ initial: "4", sm: "6" }}
+          weight={{ initial: "regular", sm: "bold" }}
+          className="text-navy-900"
+        >
+          ASSORT was established as part of a joint partnership between{" "}
+          <SmartLink
+            href="https://www.boxtribute.org"
+            className="text-inherit underline"
+          >
+            Boxtribute
+          </SmartLink>
+          ,{" "}
+          <SmartLink
+            href="https://www.distributeaid.org"
+            className="text-inherit underline"
+          >
+            Distribute Aid
+          </SmartLink>
+          ,{" "}
+          <SmartLink
+            href="https://www.hermine.global"
+            className="text-inherit underline"
+          >
+            HERMINE
+          </SmartLink>
+          , and the{" "}
+          <SmartLink
+            href="https://www.iha.help"
+            className="text-inherit underline"
+          >
+            InterEuropean Human Aid Association
+          </SmartLink>
+          .
         </Text>
-        <Image
-          src="/images/resources/arrow-down.png"
-          alt="ASSORT"
-          width={22}
-          height={38}
-        />
-        <Text size="4" align="center">
-          When the shipment arrives, they process it much faster than they
-          otherwise could. Indeed, a pilot shipment showed that using ASSORT
-          standards significantly cut down on the receiving organisation’s
-          sorting and processing time. Saving time and volunteer capacity makes
-          a big difference for grassroots aid organisations.
-        </Text>
-      </Flex>
-      <Flex
-        mx={{ md: "9", initial: "2" }}
-        gap={{ initial: "4", md: "0" }}
-        className="text-navy-800"
-        direction={{ md: "row", initial: "column" }}
-        align="center"
-        my="8"
-      >
-        <Guide />
-        <Image
-          src="/images/resources/resources_guide.png"
-          alt="ASSORT"
-          width={548}
-          height={282}
-        />
-      </Flex>
-      <Heading
-        align={{ initial: "center", lg: "left" }}
-        size={{ initial: "5", sm: "7" }}
-        weight={{ initial: "regular", sm: "bold" }}
-        className="text-navy-900"
-        mx={{ md: "9", initial: "3" }}
-      >
-        ASSORT was established as part of a joint partnership between{" "}
-        <ResourceRedirect text="Boxtribute" link="www.boxtribute.org" />,{" "}
-        <ResourceRedirect link="www.distributeaid.org" text="Distribute Aid" />,{" "}
-        <ResourceRedirect link="www.hermine.global" text="HERMINE" />, and the{" "}
-        <ResourceRedirect
-          text="InterEuropean Human Aid Association"
-          link="www.iha.help"
-        />
-      </Heading>
-      <Flex
-        direction="column"
-        align="center"
-        className="bg-red-50"
-        mt={{ md: "9", initial: "7" }}
-        py="43px"
-        px={{ initial: "3", md: "9" }}
-      >
-        <ResourcesFooter />
-      </Flex>
-    </section>
+      </Container>
+      <ResourcesFooter />
+    </>
   );
 };
 
