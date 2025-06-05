f63370037912f1dd57d6d516fcb002d38b473196
Claire Lin <chunhuilin7516@gmail.com>
2025-06-05 18:39:43 +0200

    WIP

--- src/components/about-us/AboutHero.tsx
+++ src/components/about-us/AboutHero.tsx
@@ -4,75 +4,95 @@ import { Box, Flex, Container, Section, Heading, Text } from "@radix-ui/themes";
 
 export const AboutHero: FC = () => (
   <>
-    <Section className="bg-navy-900 pb-20 relative z-0 sm:pb-12 pt-0 sm:pt-8 md:py-16">
-      {/* heading and text */}
-      <div className="max-w-7xl mx-auto px-8 pb-4 sm:pb-32 sm:px-16 md:px-24 pt-4">
+    <Section
+      position="relative"
+      width="100%"
+      pt={{ initial: "2", sm: "7", md: "9"}}
+      px={{ lg: "9" }}
+      mb="100px"
+      className="bg-navy-900"
+      style={{ paddingBottom: "120px" }}
+    >
+      <Box
+        pt={{ initial: "1", md: "4" }}
+        pb={{ initial: "2", md: "6" }}
+        px={{ initial: "4", sm: "6", md: "9" }}
+      >
         <Heading
           as="h1"
+          size={{ initial: "7", sm: "8", md: "9" }}
           weight="bold"
           align="center"
-          className="text-white pb-8 text-[32px] sm:text-[48px] md:text-[60px]"
+          className="text-white"
+          mb={{ initial: "3", sm: "4" }}
         >
           About Us
         </Heading>
         <Text
           as="p"
-          size={{
-            initial: "6",
-            sm: "7",
-            md: "8"
-          }}
+          size={{ initial: "5", sm: "6", md: "7" }}
           align="center"
           className="text-white"
         >
           We provide for basic human needs at scale by connecting communities
           and empowering people to uphold human dignity.
         </Text>
-      </div>
+      </Box>
+      <Flex
+        position="absolute"
+        bottom="-100px"
+        width="100%"
+        align="start"
+        justify="center"
+        wrap="wrap"
+        gap="5"
+        px="4"
+      >
+        <Box className="w-[96px] sm:w-[120px] md:w-[160px] lg:w-[200px]">
+          <Image
+            src={"/images/about-us/hero-1.png"}
+            alt="Hero Image"
+            width={0}
+            height={0}
+            sizes="(min-width: 1024px) 200px, (min-width: 768px) 160px, (min-width: 640px) 120px, 96px"
+            style={{ width: "100%", height: "auto", objectFit: "cover" }}
+          />
+        </Box>
+        <Box className="w-[96px] sm:w-[120px] md:w-[160px] lg:w-[200px]">
+          <Image
+            src={"/images/about-us/hero-2.png"}
+            alt="Hero Image"
+            width={0}
+            height={0}
+            sizes="(min-width: 1024px) 200px, (min-width: 768px) 160px, (min-width: 640px) 120px, 96px"
+            style={{ width: "100%", height: "auto", objectFit: "cover" }}
+          />
+        </Box>
+        <Box className="w-[96px] sm:w-[120px] md:w-[160px] lg:w-[200px]">
+          <Image
+            src={"/images/about-us/hero-3.png"}
+            alt="Hero Image"
+            width={0}
+            height={0}
+            sizes="(min-width: 1024px) 200px, (min-width: 768px) 160px, (min-width: 640px) 120px, 96px"
+            style={{ width: "100%", height: "auto", objectFit: "cover" }}
+
+          />
+        </Box>
+      </Flex>
     </Section>
-    <div className="-mt-16 sm:-mt-32 sm:px-16 z-10 relative w-full flex flex-wrap justify-center gap-2 sm:gap-3 md:gap-5 px-8">
-      <div className="flex-1 max-w-[320px]">
-        <Image
-          src="/images/about-us/hero-1.png"
-          alt="Hero 1"
-          width={0}
-          height={0}
-          sizes="(max-widh: 1024px) 30vw, 200px"
-          className="w-full h-auto rounded-xl"
-        />
-      </div>
-      <div className="flex-1 max-w-[320px]">
-        <Image
-          src="/images/about-us/hero-2.png"
-          alt="Hero 1"
-          width={0}
-          height={0}
-          sizes="(max-widh: 1024px) 30vw, 200px"
-          className="w-full h-auto rounded-xl"
-        />
-      </div>
-      <div className="flex-1 max-w-[320px]">
-        <Image
-          src="/images/about-us/hero-3.png"
-          alt="Hero 1"
-          width={0}
-          height={0}
-          sizes="(max-widh: 1024px) 30vw, 200px"
-          className="w-full h-auto rounded-xl"
-        />
-      </div>
-    </div>
     <Section>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+      <Container size="3">
         <Heading
-          as="h1"
+          as="h2"
+          size="9"
           weight="bold"
           align="center"
-          className="text-navy-900 text-[32px] sm:text-[48px] md:text-[60px] leading-tight"
+          className="text-navy-900"
         >
           Support us today, so communities are prepared for tomorrow.
         </Heading>
-      </div>
+      </Container>
     </Section>
   </>
 );
