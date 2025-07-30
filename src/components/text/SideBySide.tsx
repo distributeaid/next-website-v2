2309eddefe523653c82c98667bfb31c11b84a55e
Claire Lin <chunhuilin7516@gmail.com>
2025-07-30 18:23:03 +0200

    Fix layout and spacing in Sidebyside component for responsiveness

--- src/components/text/SideBySide.tsx
+++ src/components/text/SideBySide.tsx
@@ -1,4 +1,4 @@
-import { Flex, Section, Separator } from "@radix-ui/themes";
+import { Box, Flex, Section, Separator } from "@radix-ui/themes";
 import React, { ReactNode } from "react";
 
 interface SideBySideProps {
@@ -13,20 +13,28 @@ export const SideBySide: React.FC<SideBySideProps> = ({
   bgClass,
 }) => {
   return (
-    <Section px={{ initial: "4", sm: "9" }} className={bgClass}>
+    <Section
+      maxWidth="7xl"
+      px={{ initial: "4", md: "9" }}
+      className={bgClass}
+    >
       <Flex
-        direction={{ initial: "column", md: "row" }}
-        align="start"
+        direction={{ initial: "column", sm: "column", md: "row" }}
+        align="center"
         justify="center"
-        gap={{ initial: "4", md: "6" }}
+        gap={{ initial: "4" }}
       >
-        {left}
+        <Box width={{ initial: "100%", md: "1/2" }}>
+          {left}
+        </Box>
         <Separator
           orientation={{ initial: "horizontal", md: "vertical" }}
           size="4"
-          className="w-3/4 md:w-px lg:self-stretch lg:h-auto my-4"
+          className="w-full sm:my-4 md:w-px md:self-stretch md:h-auto md:mx-4"
         />
-        {right}
+        <Box width={{ initial: "100%", md: "1/2" }}>
+          {right}
+        </Box>
       </Flex>
     </Section>
   );
