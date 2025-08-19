1aaf60bcba4da2485df260a75942e0d70164e1f2
Michael Edquilan <michaeledquilan@gmail.com>
2025-08-19 23:49:18 +0000

    chore: fix type issue

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,12 +1,10 @@
 import ContactForm from "@/components/contact-us/ContactForm";
 import GotAidSection from "@/components/contact-us/GotAidSection";
-import ImageHeader from "@/components/contact-us/ImageHeader";
 import SupportUsSection from "@/components/contact-us/SupportUsSection";
 import VolunteerSection from "@/components/contact-us/VolunteerSection";
 const page = () => {
   return (
     <>
-      <ImageHeader />
       <ContactForm />
       <GotAidSection />
       <SupportUsSection />
