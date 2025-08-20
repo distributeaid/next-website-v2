07d64a9003762d0e37a660a8acc46a5e72599264
Michael Edquilan <michaeledquilan@gmail.com>
2025-08-20 18:17:16 +0000

    feat:created image header component for page use

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,10 +1,16 @@
 import ContactForm from "@/components/contact-us/ContactForm";
 import GotAidSection from "@/components/contact-us/GotAidSection";
+import ImageHeader from "@/components/contact-us/ImageHeader";
 import SupportUsSection from "@/components/contact-us/SupportUsSection";
 import VolunteerSection from "@/components/contact-us/VolunteerSection";
 const page = () => {
   return (
     <>
+      {/* example use of ImageHeader */}
+      {/* <ImageHeader
+        imageUrl="/images/contact-us/gift-graphic.png"
+        heading="Support Us"
+      /> */}
       <ContactForm />
       <GotAidSection />
       <SupportUsSection />
