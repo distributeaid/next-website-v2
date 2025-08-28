a1e383a557bb98ec882b24489692aadc19cccda1
Kai Katschthaler (they/he) <kai.katschthaler@gmail.com>
2025-08-28 17:01:52 +0000

    small changes to home page sections

--- src/data/home.ts
+++ src/data/home.ts
@@ -28,11 +28,11 @@ export const activities = [
       "Distribute Aid is actively working to prevent the spread of COVID-19 globally. We regularly send hygiene and PPE shipments to our European partners who continue to work to protect people in crowded refugee camps. Because COVID-19 affects everybody, our response grew as the virus spread. We now also collaborate with multiple grassroots networks in the US to support their response.",
   },
   {
-    title: "In-kind Donations",
+    title: "In-Kind Donations",
     region: "Global",
     image: inKindDonations,
     description:
-      "Distribute Aid runs a successful in-kind donation programme where we connect companies with aid organisations. Through our network of hundreds of charities, we can handle coordinating large volumes of donations. To date, we have secured over 5 million items through our in-kind donor programme. Email <donate-aid@distributeaid.org> if you can donate in-kind.",
+      "Distribute Aid runs a successful in-kind donation programme where we connect companies with aid organisations. Through our network of hundreds of charities, we can handle coordinating large volumes of donations. To date, we have secured over 5 million items through our in-kind donor programme. Email <donate-aid@distributeaid.org> if you can donate in kind.",
   },
   {
     title: "Fort Pickett",
