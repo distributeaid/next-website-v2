da97ed8f102840f8b19b1b7f2041ce96df542b2b
Graham Hall <graham@ghall.dev>
2025-09-04 12:14:01 -0400

    Add copy section to /donate

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -1,12 +1,14 @@
 import DonateGrid from "@/components/donate/DonateGrid";
 import FUND_RAISERS from "@/data/donate.json";
 import HeroSection from "@/components/donate/HeroSection";
+import CopySection from "@/components/donate/CopySection";
 import DonateLandingText from "@/components/donate/DonateLandingText";
 
 const Page = () => {
   return (
     <>
       <HeroSection />
+      <CopySection />
       {FUND_RAISERS && FUND_RAISERS.length > 0 && (
         <DonateGrid funds={FUND_RAISERS} />
       )}
---
+++ src/components/donate/CopySection.tsx
@@ -0,0 +1,27 @@
+import { Box, Text } from "@radix-ui/themes";
+
+const CopySection = () => (
+  <Box
+    pt={{
+      initial: "90px",
+      sm: "80px",
+      md: "64px",
+    }}
+    pb="64px"
+    px={{
+      initial: "20px",
+      sm: "0",
+    }}
+    maxWidth={{ sm: "80%", md: "70%" }}
+    mx="auto"
+  >
+    <Text as="p" align={"center"} size={{ initial: "5", md: "6" }}>
+      Distribute Aid ensures every dollar and diaper donated is put to good use.
+      Our logistics management system reduces wasted items and shipping costs,
+      including carbon emissions, by significant margins. We don&apos;t pay our
+      executives lavish salaries, and we don&apos;t spend money on advertising.
+    </Text>
+  </Box>
+);
+
+export default CopySection;
