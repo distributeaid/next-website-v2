b57dc7f1db7536110dda9aebb54300ad29b19e57
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-04 17:18:33 -0500

    Prettier

--- src/components/responses/hrt-toolkit/HrtToolkitHero.tsx
+++ src/components/responses/hrt-toolkit/HrtToolkitHero.tsx
@@ -4,7 +4,7 @@ import { Section } from "@radix-ui/themes";
 export const HrtToolkitHero: FC = () => (
   <>
     <Section>
-    	This is where the HRT Harm Reduction Toolkit page will be.
+      This is where the HRT Harm Reduction Toolkit page will be.
     </Section>
   </>
 );
