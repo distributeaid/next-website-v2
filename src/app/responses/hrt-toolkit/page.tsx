14a0ad7c5c9dab903f0952a7b79d687fc3cd213b
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-04 12:36:56 -0500

    Placeholder page

---
+++ src/app/responses/hrt-toolkit/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const HrtToolkit: FC = () => {
+  return (
+    <main>
+      This is where the HRT Harm Reduction Toolkit page will be!
+    </main>
+  );
+};
+
+export default HrtToolkit;
