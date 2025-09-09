191dacef898251c396e08798fb39a22e98978fa9
Claire Lin <chunhuilin7516@gmail.com>
2025-09-09 22:54:25 +0200

    chore: format ContactPageTitle with Prettier

--- src/components/contact-us/ContactPageTitle.tsx
+++ src/components/contact-us/ContactPageTitle.tsx
@@ -1,7 +1,5 @@
 import { PageTitle } from "../resources/assort/PageTitle";
 
 export const ContactPageTitle = () => {
-  return (
-    <PageTitle title="CONTACT US" bgColor="navy-900" textColor="white" />
-  );
+  return <PageTitle title="CONTACT US" bgColor="navy-900" textColor="white" />;
 };
