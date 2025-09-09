272f00e9aee13ffc969c863dcf86b02730d35b74
Claire Lin <chunhuilin7516@gmail.com>
2025-09-09 22:23:53 +0200

    Add ContactPageTitle component

---
+++ src/components/contact-us/ContactPageTitle.tsx
@@ -0,0 +1,7 @@
+import { PageTitle } from "../resources/assort/PageTitle";
+
+export const ContactPageTitle = () => {
+  return (
+    <PageTitle title="CONTACT US" bgColor="navy-900" textColor="white" />
+  );
+};
