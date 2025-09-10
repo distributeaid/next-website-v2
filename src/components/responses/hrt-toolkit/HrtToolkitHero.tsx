f13f3ee559fdd9608d2ed99a46bc9602d4ec8e7c
Aminata Sow <78930639+mina-js@users.noreply.github.com>
2025-09-09 21:06:47 -0400

    hrt toolkit hero

--- src/components/responses/hrt-toolkit/HrtToolkitHero.tsx
+++ src/components/responses/hrt-toolkit/HrtToolkitHero.tsx
@@ -1,10 +1,30 @@
 import { FC } from "react";
-import { Section } from "@radix-ui/themes";
+import { Container, Flex, Heading, Section, Text } from "@radix-ui/themes";
 
 export const HrtToolkitHero: FC = () => (
   <>
+    <Section position="relative" width="100%" size="1" className="bg-navy-900">
+      <Container>
+        <Heading
+          as="h1"
+          size="9"
+          weight="bold"
+          align="center"
+          className="text-white"
+        >
+          HRT KITS PROJECT
+        </Heading>
+      </Container>
+    </Section>
+
     <Section>
-      This is where the HRT Harm Reduction Toolkit page will be.
+      <Container size="3">
+        <Text as="p" size="5" align="center" className="text-navy-800">
+          The Aid Standards for Sorting (ASSORT) are a set of interoperability
+          standards for humanitarian aid that are designed to save grassroots
+          organisations time, effort, and money.
+        </Text>
+      </Container>
     </Section>
   </>
 );
