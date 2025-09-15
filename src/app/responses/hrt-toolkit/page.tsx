6f3fed8f4388bbfb905d0dd370efcdc8d5b55428
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-15 16:31:20 -0500

    Collapsible and styling

--- src/app/responses/hrt-toolkit/page.tsx
+++ src/app/responses/hrt-toolkit/page.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import { HrtToolkitHero } from "@/components/responses/hrt-toolkit/HrtToolkitHero";
+import { GetInvolved } from "@/components/responses/hrt-toolkit/GetInvolved";
 
 const HrtToolkit: FC = () => {
   return (
     <main>
       <HrtToolkitHero />
+      <GetInvolved />
     </main>
   );
 };
---
+++ src/components/responses/hrt-toolkit/GetInvolved.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { FC, useState } from "react";
+import { Button, Heading, Section, Text } from "@radix-ui/themes";
+import { Collapsible } from "radix-ui";
+import Image from "next/image";
+import fortPickett from "../../../../public/images/home/fort-pickett.jpg";
+
+// Can't get the @radix-ui/react-collapsible package to work
+// So I installed radix-ui for now. Don't forget to delete it later
+
+export const GetInvolved = () => {
+  return (
+    <>
+      <Section>
+        <Heading align="center" className="text-[#082B76] mb-8" size="8">How To Get Involved</Heading>
+
+        <div className="flex justify-center">
+          <GetInvolvedCollapse
+            title="Frontline Groups"
+            image={fortPickett}
+          >
+            This is the information for frontline groups.
+          </GetInvolvedCollapse>
+          <GetInvolvedCollapse
+            title="In-kind Donations"
+            image={fortPickett}
+          >
+            This is the information for in-kind donations.
+          </GetInvolvedCollapse>
+        </div>
+      </Section>
+    </>
+  );
+};
+
+const GetInvolvedCollapse = ({title, children, image}) : {title: string, children?: ReactNode} => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Collapsible.Root
+      className="get-involved bg-[#C5CFE4] rounded-md m-4"
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <Image
+        src={image}
+        className="w-full h-auto rounded-t-md"
+        alt={title}
+      />
+      <div className="p-4">
+        <div className="mb-4">
+          <Text size="7" className="uppercase text-[#082B76] mb-6">{title}</Text>
+        </div>
+        <Collapsible.Trigger asChild>
+          <Button
+            size="2"
+            mt="auto"
+            className="bg-navy-600 hover:bg-navy-500"
+          >More Info</Button>
+        </Collapsible.Trigger>
+        <Collapsible.Content>
+          {children}
+        </Collapsible.Content>
+      </div>
+    </Collapsible.Root>
+  )
+}
