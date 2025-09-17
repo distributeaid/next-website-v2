5f83e3ec003379256154df9c2bba576daf28a6db
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-17 12:01:48 -0500

    How It Works component

--- src/app/responses/hrt-toolkit/page.tsx
+++ src/app/responses/hrt-toolkit/page.tsx
@@ -1,10 +1,12 @@
 import React, { FC } from "react";
 import { HrtToolkitHero } from "@/components/responses/hrt-toolkit/HrtToolkitHero";
+import { HowItWorks } from "@/components/responses/hrt-toolkit/HowItWorks";
 
 const HrtToolkit: FC = () => {
   return (
     <main>
       <HrtToolkitHero />
+      <HowItWorks />
     </main>
   );
 };
