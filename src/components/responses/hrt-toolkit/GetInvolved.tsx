bbefa5591794b55f0185489335b4c70f1be8fc63
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-17 11:16:59 -0500

    Clean up

--- src/components/responses/hrt-toolkit/GetInvolved.tsx
+++ src/components/responses/hrt-toolkit/GetInvolved.tsx
@@ -14,10 +14,14 @@ export const GetInvolved: FC = () => {
         <div className="flex justify-center flex-wrap">
           <GetInvolvedBox title="Frontline Groups" image={fortPickett}>
             <div className="font-bold  mb-2">
-              This is the information for frontline groups. Lorem ipsum dolor sit amet, 
-              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              This is the information for frontline groups. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+              ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+              officia deserunt mollit anim id est laborum.
             </div>
             <Button
               size="2"
@@ -60,9 +64,7 @@ const GetInvolvedBox: FC<GetInvolvedBoxType> = ({ title, children, image }) => (
           {title}
         </Text>
       </div>
-      <div>
-          {children}
-      </div>
+      <div>{children}</div>
     </div>
   </div>
 );
