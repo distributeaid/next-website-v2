6eedbd5a0fbda7d7309b3e450a4bfd632cd8e18f
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-18 12:37:21 -0500

    Switch to more radix components

--- src/components/responses/hrt-toolkit/HowItWorks.tsx
+++ src/components/responses/hrt-toolkit/HowItWorks.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Heading, Section } from "@radix-ui/themes";
+import { Box, Flex, Heading, Section } from "@radix-ui/themes";
 import Image from "next/image";
 import graph from "../../../../public/images/responses/hrt-toolkit/how-it-works-graph.png";
 
@@ -9,17 +9,17 @@ export const HowItWorks: FC = () => (
       <Heading as="h2" align="center" className="text-navy-800" size="8">
         How It Works
       </Heading>
-      <div className="flex justify-center pb-4">
+      <Flex justify="center" p="4">
         <Image
           src={graph}
           className="w-5/6"
           alt="Materials purchased with monetary donations and obtained through in-kind donations are assembled to make the kits. The kits are sent to frontline groups who distribute them in their area."
         />
-      </div>
-      <div className="font-bold text-center text-lg">
+      </Flex>
+      <Box className="font-bold text-center text-lg">
         Good to know: Kits are never distributed directly to people but only
         through frontline organizations!
-      </div>
+      </Box>
     </Section>
   </>
 );
