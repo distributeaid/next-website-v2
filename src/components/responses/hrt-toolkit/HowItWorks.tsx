40afc58641523d55fbc213a110ce523431e8c462
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-18 12:39:52 -0500

    Clean up

--- src/components/responses/hrt-toolkit/HowItWorks.tsx
+++ src/components/responses/hrt-toolkit/HowItWorks.tsx
@@ -4,22 +4,20 @@ import Image from "next/image";
 import graph from "../../../../public/images/responses/hrt-toolkit/how-it-works-graph.png";
 
 export const HowItWorks: FC = () => (
-  <>
-    <Section>
-      <Heading as="h2" align="center" className="text-navy-800" size="8">
-        How It Works
-      </Heading>
-      <Flex justify="center" p="4">
-        <Image
-          src={graph}
-          className="w-5/6"
-          alt="Materials purchased with monetary donations and obtained through in-kind donations are assembled to make the kits. The kits are sent to frontline groups who distribute them in their area."
-        />
-      </Flex>
-      <Box className="font-bold text-center text-lg">
-        Good to know: Kits are never distributed directly to people but only
-        through frontline organizations!
-      </Box>
-    </Section>
-  </>
+  <Section>
+    <Heading as="h2" align="center" className="text-navy-800" size="8">
+      How It Works
+    </Heading>
+    <Flex justify="center" p="4">
+      <Image
+        src={graph}
+        className="w-5/6"
+        alt="Materials purchased with monetary donations and obtained through in-kind donations are assembled to make the kits. The kits are sent to frontline groups who distribute them in their area."
+      />
+    </Flex>
+    <Box className="font-bold text-center text-lg">
+      Good to know: Kits are never distributed directly to people but only
+      through frontline organizations!
+    </Box>
+  </Section>
 );
