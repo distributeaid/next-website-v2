8586aa0bafdbd9dffb5429697dde00c361880e2e
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-24 11:21:17 -0500

    Skeleton

---
+++ src/components/responses/hrt-toolkit/HrtFaq.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Section } from "@radix-ui/themes";
+
+export const HrtFaq: FC = () => (
+  <>
+    <Section>
+      This is where the HRT Frequently Asked Questions will go
+    </Section>
+  </>
+);
