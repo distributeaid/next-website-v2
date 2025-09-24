d06f14b282c6a37ee7598d67c7d72c31f26250d5
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-09-24 11:55:02 -0500

    Creates Get Involved component

--- src/app/responses/hrt-toolkit/page.tsx
+++ src/app/responses/hrt-toolkit/page.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { HrtToolkitHero } from "@/components/responses/hrt-toolkit/HrtToolkitHero";
+import { GetInvolved } from "@/components/responses/hrt-toolkit/GetInvolved";
 import { HowItWorks } from "@/components/responses/hrt-toolkit/HowItWorks";
 
 const HrtToolkit: FC = () => {
@@ -7,6 +8,7 @@ const HrtToolkit: FC = () => {
     <main>
       <HrtToolkitHero />
       <HowItWorks />
+      <GetInvolved />
     </main>
   );
 };
