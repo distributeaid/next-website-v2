0207d64048f5e4cf9d32e3a22b93864a7f1c21f1
Claire Lin <chunhuilin7516@gmail.com>
2025-10-07 14:11:45 +0200

    content(about-us): update mission and vision text

--- src/components/about-us/AboutOurMissionVision.tsx
+++ src/components/about-us/AboutOurMissionVision.tsx
@@ -2,8 +2,8 @@ import { Box, Heading, Text } from "@radix-ui/themes";
 import { FC } from "react";
 
 export const data = {
-  mission: `We are re-imagining humanitarian aid delivery. We aim to help move humanitarian aid to where it is needed most, create an efficient and time-saving system for shipments of aid, and lower carbon emissions for humanitarian aid. \n\nDistribute Aid has the end goal of producing a platform for the use of aid collection and service, providing groups that will incorporate all of these aims seamlessly. \n\nWe offer support to a huge network of grassroots organizations working within the Refugee Aid movement in Europe, and COVID-19 response groups in Europe and the US`,
-  vision: `Distribute Aid wants to bring more transparency to all groups involved around what donations are on offer, and make it easier to see what is needed where, which also prevents waste. \n\nCreating a platform for aid delivery will connect hundreds of independent groups working in the same field, for the same greater cause but who currently have little oversight. \n\nThis platform will also provide a way to collect data on regional needs, providing a wider overview of needs and assisting in securing in-kind donations to a scale never before possible!`,
+  mission: `Distribute Aid is a grassroots-to-grassroots humanitarian aid delivery team. We help communities survive and recover from disasters. We focus on three main types of relief work: sourcing shipments of humanitarian aid, coordinating its transportation to affected areas, and providing local people with the services and tools they need to rebuild their lives.`,
+  vision: `Powered by volunteers, we use open-source tech and a needs-first approach (i.e. asking people what they actually need before we send shipments) to minimize waste and maximize the impact of every donation. Our diverse team is composed of community organizers, logistics experts, and tech developers. We’re a nonprofit committed to human rights and dignity for everyone, everywhere.`,
 };
 
 type Props = {
