51eaeda8664988e91df9be8c00b9ac624185bada
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2025-10-10 10:53:58 +0100

    feat: add what we do section and cards

---
+++ src/components/service-card/ServiceCard.tsx
@@ -0,0 +1,58 @@
+import { Box, Heading, Text } from "@radix-ui/themes";
+import React from "react";
+import Image from "next/image";
+
+interface ServiceCardProps {
+  img: string;
+  title: string;
+  description: string;
+  alt: string;
+}
+
+const ServiceCard: React.FC<ServiceCardProps> = ({
+  img,
+  alt,
+  title,
+  description,
+}) => {
+  return (
+    <Box>
+      <Box
+        className={"h-[288px] md:h-[400px] lg:h-[432px]"}
+        width="100%"
+        overflow="hidden"
+        position="relative"
+      >
+        <Image
+          src={img}
+          alt={alt}
+          fill
+          className="object-cover rounded-2xl absolute"
+          loading="lazy"
+          sizes="100vw"
+        />
+      </Box>
+
+      <Heading
+        as="h2"
+        align="center"
+        weight="bold"
+        className="text-navy-900"
+        mt="6"
+      >
+        {title}
+      </Heading>
+      <Text
+        align="center"
+        weight="regular"
+        as="p"
+        className="text-navy-900 px-2 md:px-9"
+        mt="3"
+      >
+        {description}
+      </Text>
+    </Box>
+  );
+};
+
+export default ServiceCard;
