9e533025a29acf39608f929669cb2cf9cc157959
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-10-15 14:41:35 -0500

    Additional styling fixes

--- src/components/responses/hrt-toolkit/HrtKitsInNumbers.tsx
+++ src/components/responses/hrt-toolkit/HrtKitsInNumbers.tsx
@@ -1,65 +1,77 @@
 import { FC } from "react";
-import { Box, Button, Em, Flex, Heading, Section, Text } from "@radix-ui/themes";
+import {
+  Box,
+  Button,
+  Em,
+  Flex,
+  Heading,
+  Section,
+  Text,
+} from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 import { HRTNumbersCard, HrtNumbersCardProps } from "./HrtNumbersCard";
 
 export const HrtKitsInNumbers: FC = () => {
   return (
-    <>
-      <Section>
+    <Section p="0">
+      <Flex gap="2" className="flex-wrap" align="center" justify="center">
+        <HRTNumbersCard
+          amount={1000}
+          background="circle-green"
+          color="white"
+          direction="column"
+          icon="/images/responses/hrt-toolkit/medical-kit.svg"
+          iconAlt="medical kit"
+        >
+          <Text as="p" size="8" weight="light">
+            PARTIAL KITS
+          </Text>
+          <Text as="p" size="5" weight="bold" align="center">
+            <Em>donate to get remaining supplies!</Em>
+          </Text>
+          <Button className="bg-white text-navy-800" size="4" asChild>
+            <Link
+              href="https://www.omprakash.org/global/distribute-aid/crowdfund/hrt---harm-reduction-toolkit"
+              target={"_blank"}
+              rel="noopener noreferrer"
+            >
+              Donate
+            </Link>
+          </Button>
+        </HRTNumbersCard>
         <Flex
-          gap="2"
           height="360px"
-          className="flex-wrap"
-          align="center"
-          justify="center"
+          justify="between"
+          direction="column"
         >
           <HRTNumbersCard
             amount={1000}
-            background="circle-green"
-            color="white"
-            direction="column"
-            icon="/images/responses/hrt-toolkit/medical-kit.svg"
+            background="navy-300"
+            color="navy-800"
+            direction="row"
+            icon="/images/responses/hrt-toolkit/medical-kit-blue.svg"
             iconAlt="medical kit"
           >
-            <Text as="p" size="7" weight="light">PARTIAL KITS</Text>
-            <Text as="p" size="5" weight="bold" align="center"><Em>donate to get remaining supplies!</Em></Text>
-            <Button className="bg-white text-navy-800" size="4" asChild>
-              <Link
-                href="https://www.omprakash.org/global/distribute-aid/crowdfund/hrt---harm-reduction-toolkit"
-                target={"_blank"}
-                rel="noopener noreferrer"
-              >
-                Donate
-              </Link>
-            </Button>
+            <Text size="6" weight="light">
+              KITS PRODUCED SO FAR
+            </Text>
           </HRTNumbersCard>
-          <Flex height="100%" justify="between" align="between" direction="column">
-            <HRTNumbersCard
-              amount={1000}
-              background="navy-300"
-              color="navy-800"
-              direction="row"
-              icon="/images/responses/hrt-toolkit/medical-kit-blue.svg"
-              iconAlt="medical kit"
-            >
-              <Text size="6" weight="light">KITS PRODUCED SO FAR</Text>
-            </HRTNumbersCard>
-            <HRTNumbersCard
-              amount={1000}
-              background="navy-800"
-              color="white"
-              direction="row"
-              icon="/images/responses/hrt-toolkit/money-dollar.svg"
-              iconAlt="money"
-              amountPrefix="$"
-            >
-              <Text size="4" weight="light">SAVED FOR THE TRANS COMMUNITY</Text>
-            </HRTNumbersCard>
-          </Flex>
+          <HRTNumbersCard
+            amount={10000}
+            background="navy-800"
+            color="white"
+            direction="row"
+            icon="/images/responses/hrt-toolkit/money-dollar.svg"
+            iconAlt="money"
+            amountPrefix="$"
+          >
+            <Text size="4" weight="light">
+              SAVED FOR THE TRANS COMMUNITY
+            </Text>
+          </HRTNumbersCard>
         </Flex>
-      </Section>
-    </>
+      </Flex>
+    </Section>
   );
 };
