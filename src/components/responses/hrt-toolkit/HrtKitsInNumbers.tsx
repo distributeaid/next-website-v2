5327c8e222ad28ec295196f6b6bab639590ae059
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-10-15 13:40:14 -0500

    Boxes with stats

--- src/components/responses/hrt-toolkit/HrtKitsInNumbers.tsx
+++ src/components/responses/hrt-toolkit/HrtKitsInNumbers.tsx
@@ -1,40 +1,60 @@
 import { FC } from "react";
-import { Flex, Heading, Section } from "@radix-ui/themes";
+import { Box, Button, Em, Flex, Heading, Section, Text } from "@radix-ui/themes";
 import Image from "next/image";
+import Link from "next/link";
 import { HRTNumbersCard, HrtNumbersCardProps } from "./HrtNumbersCard";
-
-const placeholderData: HrtNumbersCardProps[] = [
-  {
-    icon: "placeholder",
-    amount: 15,
-    direction: "column",
-    background: "navy-300",
-    color: "navy-800",
-  },
-  {
-    icon: "placeholder2",
-    amount: 30,
-    background: "circle-green",
-    color: "white",
-  },
-  { icon: "placeholder3", amount: 100, background: "navy-800", color: "white" },
-];
 
 export const HrtKitsInNumbers: FC = () => {
   return (
     <>
       <Section>
         <Flex
-          direction="column"
-          gap="4"
-          height="120px"
+          gap="2"
+          height="360px"
           className="flex-wrap"
           align="center"
-          wrap="wrap"
+          justify="center"
         >
-          {placeholderData.map((data, idx) => (
-            <HRTNumbersCard key={`hrt_numbers_card_${idx}`} {...data} />
-          ))}
+          <HRTNumbersCard
+            amount={1000}
+            background="circle-green"
+            color="white"
+            direction="column"
+            icon="/images/responses/hrt-toolkit/medical-kit.svg"
+          >
+            <Text as="p" size="7" weight="light">PARTIAL KITS</Text>
+            <Text as="p" size="5" weight="bold" align="center"><Em>donate to get remaining supplies!</Em></Text>
+            <Button className="bg-white text-navy-800" size="4" asChild>
+              <Link
+                href="https://www.omprakash.org/global/distribute-aid/crowdfund/hrt---harm-reduction-toolkit"
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                Donate
+              </Link>
+            </Button>
+          </HRTNumbersCard>
+          <Flex height="100%" justify="between" align="between" direction="column">
+            <HRTNumbersCard
+              amount={1000}
+              background="navy-300"
+              color="navy-800"
+              direction="row"
+              icon="/images/responses/hrt-toolkit/medical-kit-blue.svg"
+            >
+              <Text size="6" weight="light">KITS PRODUCED SO FAR</Text>
+            </HRTNumbersCard>
+            <HRTNumbersCard
+              amount={1000}
+              background="navy-800"
+              color="white"
+              direction="row"
+              icon="/images/responses/hrt-toolkit/money-dollar.svg"
+              amountPrefix="$"
+            >
+              <Text size="4" weight="light">SAVED FOR THE TRANS COMMUNITY</Text>
+            </HRTNumbersCard>
+          </Flex>
         </Flex>
       </Section>
     </>
--- src/components/responses/hrt-toolkit/HrtNumbersCard.tsx
+++ src/components/responses/hrt-toolkit/HrtNumbersCard.tsx
@@ -1,29 +1,46 @@
-import { FC } from "react";
-import { Section, Text } from "@radix-ui/themes";
+import { FC, ReactNode } from "react";
+import { Box, Flex, Section, Text } from "@radix-ui/themes";
+import Image from "next/image";
 
 export type HrtNumbersCardProps = {
   icon: string;
   amount: number;
   amountPrefix?: string;
-  cta?: string;
-  buttonLabel?: string;
-  buttonLink?: string;
   direction?: "column" | "row";
   background: string;
   color: string;
+  children?: ReactNode;
 };
 
 export const HRTNumbersCard: FC<HrtNumbersCardProps> = ({
   direction,
   amount,
+  amountPrefix,
   background,
   color,
+  children,
+  icon,
 }) => {
   return (
-    <div
-      className={`w-[100px] text-${color} bg-${background} flex flex-col justify-center items-center h-${direction === "column" ? "full" : "1/2"} `}
+    <Flex
+      className={`text-${color} bg-${background} rounded-xl box-border`}
+      direction={direction}
+      align="center"
+      mx="4"
+      p="2"
+      height={direction === "column" ? "100%" : "45%"}
+      maxWidth="300px"
     >
-      <Text as="p">here ${amount}</Text>
-    </div>
+      <Image src={icon} width="100" height="100"/>
+      <Box>
+        <Text as="p" size="9" align={direction === "column" ? "center" : "left"} weight="bold">
+          {amountPrefix}
+          {amount.toLocaleString()}
+        </Text>
+        <Box align={direction === "column" ? "center" : "left"}>
+          {children}
+        </Box>
+      </Box>
+    </Flex>
   );
 };
