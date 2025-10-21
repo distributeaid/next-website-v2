00970defb69a926fc142e9f5b92993353ce7f529
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2025-10-21 11:05:27 +0100

    feat: add our impact component
    # Conflicts:
    #	src/app/about-us/page.tsx

---
+++ src/data/ourImpact.tsx
@@ -0,0 +1,44 @@
+import { IconsRepository } from "./icons-repository";
+
+export const ImpactData = [
+  {
+    icon: <IconsRepository.MoneyFilledIcon />,
+    value: "4.4M+",
+    label: "Cash Value of Aid",
+  },
+  {
+    icon: <IconsRepository.TruckIcon />,
+    value: "4.4M+",
+    label: "Truckloads of Aid",
+  },
+  {
+    icon: <IconsRepository.MedicalKitBoldIcon />,
+    value: "4.4M+",
+    label: "Medical Supplies",
+  },
+  {
+    icon: <IconsRepository.BowlFoodFillIcon />,
+    value: "4.4M+",
+    label: "Tons of Food",
+  },
+  {
+    icon: <IconsRepository.ShirtIcon />,
+    value: "4.4M+",
+    label: "Clothing Items",
+  },
+  {
+    icon: <IconsRepository.TentFilledIcon />,
+    value: "4.4M+",
+    label: "Tents/Shelters",
+  },
+  {
+    icon: <IconsRepository.ShakingHandsIcon />,
+    value: "142+",
+    label: "Partners Worked With",
+  },
+  {
+    icon: <IconsRepository.PackageIcon />,
+    value: "4.4M+",
+    label: "Donors",
+  },
+];
