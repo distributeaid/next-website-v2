ab5980eba009481a306b784ad23529f0d276e474
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-10-30 12:56:57 -0500

    Update FAQs

--- src/components/responses/hrt-toolkit/HrtFaq.tsx
+++ src/components/responses/hrt-toolkit/HrtFaq.tsx
@@ -5,38 +5,160 @@ import { FaArrowDown } from "react-icons/fa";
 
 const faqs = [
   [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum?",
-    "Here is where we'd put an answer.",
+    "What is HRT?",
+    <>
+      <Text as="p" className="mb-3">
+        <strong>A:</strong> HRT is short for Hormone Replacement Therapy, which
+        is also known as Gender Affirming Hormone Therapy (GAHT). HRT also
+        happens to be the beginning letters of <strong>H</strong>arm{" "}
+        <strong>R</strong>eduction <strong>T</strong>oolkit. So we’re playing a
+        bit with words and acronyms here.
+      </Text>
+
+      <Text as="p">
+        Gender Affirming Hormone Therapy has been linked to better mental health
+        outcomes for trans and nonbinary people, lowering suicidal ideation and
+        increasing happiness. You can find out more about this via the{" "}
+        <a href="https://ustranssurvey.org">2022 U.S. Trans Survey</a>."
+      </Text>
+    </>,
   ],
   [
-    "Here is where we'd put a question?",
-    "Here is where we'd put an answer - lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    "What’s in an HRT kit? Are you distributing hormones?",
+    <>
+      <Text as="p" className="mb-3">
+        <strong>A:</strong> We do not distribute hormones. We only assemble the
+        sundries needed to administer hormone replacement therapy. These include
+        syringes, needles in various gauges, disinfectant wipes, and a sharps
+        disposal container to safely dispose of needles. Each kit provides
+        supplies for one trans person for one year.
+      </Text>
+      <Text as="p">
+        A regular, accessible, affordable supply of clean needles is essential
+        to reduce the harms associated with needle reuse and sharing.
+      </Text>
+    </>,
   ],
-  ["Here is where we'd put a question?", "Here is where we'd put an answer."],
+
+  [
+    "Why do trans people need these things and why can’t they just buy them?",
+    <>
+      <Text as="p" className="mb-3">
+        <strong>A:</strong> While $65 for yearly supplies may not seem like a
+        lot of money for many people, it’s an expense that many trans people who
+        are already more likely to be in precarious economical situations cannot
+        cover. This may force them to choose between important medical care and
+        buying groceries. They also generally end up paying much more than $65
+        because they cannot afford to buy supplies in one go, which drives up
+        the price.
+      </Text>
+      <Text as="p">
+        Thanks to our extensive aid partners and grassroots network, Distribute
+        Aid can source these supplies far more efficiently, effectively bringing
+        the actual price per kit down further to $10 per kit.
+      </Text>
+    </>,
+  ],
+
+  [
+    "Where are the kits being assembled?",
+    <>
+      <Text as="p">
+        <strong>A:</strong> Distribute Aid sources the materials (in-kind
+        donations or financial donations that enable us to buy the resources),
+        which are then assembled by volunteers in various production hubs around
+        the southern US. The kits are then sent to various established frontline
+        partners, such as community organisations or mutual aid hubs, who then
+        distribute them to trans people within their communities.
+      </Text>
+    </>,
+  ],
+
+  [
+    "Which areas are you covering?",
+    <>
+      <Text as="p">
+        <strong>A:</strong> We’re currently covering areas in the southern US.
+        We may consider extending coverage as this project continues to grow.
+      </Text>
+    </>,
+  ],
+
+  [
+    "I’m a trans person in need of an HRT Harm Reduction Toolkit. How do I get one?",
+    <>
+      <Text as="p" className="mb-3">
+        <strong>A:</strong> If you are in need of an HRT Harm Reduction Toolkit,
+        we recommend getting in touch with your local frontline organisation,
+        mutual aid hub, etc. and asking them if they want to get involved.
+        Frontline groups can then reach out to us to sign up to become
+        distributors for these kits.
+      </Text>
+      <Text as="p">
+        We do not advertise the kits to individuals! Please do not reach out to
+        Distribute Aid directly to get a kit.
+      </Text>
+    </>,
+  ],
+
+  [
+    "How did this project start?",
+    <>
+      <Text as="p" className="mb-3">
+        <strong>A:</strong> The escalating attacks on transgender people in the
+        US after November 2024 led to our decision to do something to
+        specifically support trans people. We{" "}
+        <a href="https://bsky.app/profile/borderless.bsky.social/post/3lda7sebxqk2e">
+          started our campaign
+        </a>
+        in December 2024 and fairly quickly raised an impressive amount of
+        funds, which we then put to use sourcing the materials needed to safely
+        administer gender-affirming hormone therapy or Hormone Replacement
+        Therapy (HRT). Thanks to our established network of partners and our
+        experience distributing aid worldwide we can source ingredients at a
+        much lower cost. In addition, we collect in-kind donations as well. The
+        toolkits are assembled by volunteers in various hubs and are then
+        distributed to trans people in need for free by frontline organisations.
+      </Text>
+
+      <Text as="p">
+        We plan to keep this project going as long as we can and as long as it’s
+        needed and will be looking into extending the reach of the project
+        whenever possible.
+      </Text>
+    </>,
+  ],
+
+  [
+    "I’m a journalist and want to cover this project. Who do I contact?",
+    <>
+      <Text as="p">
+        <strong>A:</strong> [tbd]
+      </Text>
+    </>,
+  ],
 ];
 
 export const HrtFaq: FC = () => (
-  <>
-    <Section p="0">
-      <Heading as="h2" align="center" size="8">
-        FAQ <FaArrowDown className="inline" />
-      </Heading>
-      <Container px="8" py="4" className="bg-navy-300">
-        {faqs?.map((item, index) => (
-          <Box key={`faq-${index}`} my="1">
-            <details>
-              <summary>
-                <Text weight="bold" size="4">
-                  Q: "{item[0]}"
-                </Text>
-              </summary>
-              <Text as="div" size="3" className="pl-8">
-                A: {item[1]}
+  <Section p="0">
+    <Heading as="h2" align="center" size="8">
+      FAQ <FaArrowDown className="inline" />
+    </Heading>
+    <Container px="8" py="4" className="bg-navy-300">
+      {faqs?.map((item, index) => (
+        <Box key={`faq-${index}`} my="1">
+          <details>
+            <summary>
+              <Text weight="bold" size="4">
+                Q: "{item[0]}"
               </Text>
-            </details>
-          </Box>
-        ))}
-      </Container>
-    </Section>
-  </>
+            </summary>
+            <Box as="div" className="pl-8">
+              {item[1]}
+            </Box>
+          </details>
+        </Box>
+      ))}
+    </Container>
+  </Section>
 );
