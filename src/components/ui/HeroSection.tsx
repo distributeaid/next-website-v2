5e5d94677645b4f23bfc5f229f809fd2639654e2
Graham Hall <graham@ghall.dev>
2025-11-05 14:16:14 -0500

    finish desktop layout for hero

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -1,6 +1,7 @@
 import { Flex, Heading, Button, Box, Link } from "@radix-ui/themes";
 import Image from "next/image";
 import siteSettings from "@/data/site-settings.json";
+import TextMark from "../brand/TextMark";
 
 interface HeroSectionProps {
   heading: string;
@@ -10,7 +11,7 @@ interface HeroSectionProps {
   buttons: {
     label: string;
     href: string;
-    invertColors?: boolean;
+    bgWhite?: boolean;
     targetBlank?: boolean;
   }[];
 }
@@ -23,12 +24,10 @@ const HeroSection = ({
   buttons,
 }: HeroSectionProps) => {
   return (
-    <>
-      {/* blue bg */}
+    <Box>
       <Flex
-        mb={{ initial: "30%", sm: "10%" }}
+        mb={{ initial: "30%", sm: "18%" }}
         height={"550px"}
-        // color also used in sponsor section, so maybe save it as background?
         style={{ background: "var(--blue-background)" }}
         className="shadow-xl"
       >
@@ -47,23 +46,24 @@ const HeroSection = ({
             width={{ initial: "100%", sm: "70%" }}
             className="lg:rounded-br-[32px] overflow-hidden"
           >
-            <Image fill src={imgSrc} alt={imgAlt} />
+            <Image fill src={imgSrc} alt={imgAlt} className="object-cover" />
           </Box>
-          {/* both support and ship */}
+
           <Flex
             direction={"column"}
             position={"relative"}
-            bottom={"-209px"}
+            bottom="0"
+            justify="end"
             left={{ lg: "-27%" }}
-            gap={"7"}
+            gap={"5"}
+            height={{ lg: "733px" }}
             width={"100%"}
           >
-            {/* support us */}
-
+            <TextMark width="200px" />
             <Heading
               as="h1"
               size={{ initial: "8", sm: "9" }}
-              className="text-white"
+              className="text-white leading-[4.5rem]"
               align={{ initial: "center", sm: "left" }}
             >
               {heading.split("\n").map((line) => (
@@ -73,8 +73,7 @@ const HeroSection = ({
                 </span>
               ))}
             </Heading>
-
-            {/* ship aid */}
+            {/* link container */}
             <Flex
               p={{ initial: "6", sm: "5", lg: "7" }}
               direction={{ initial: "column", sm: "row", lg: "column" }}
@@ -83,7 +82,6 @@ const HeroSection = ({
               height={{ initial: "auto", sm: "130px", lg: "184px" }}
               gap={{ initial: "3", sm: "1", lg: "5" }}
               align={{ sm: "center", lg: "start" }}
-              mx={{ initial: "auto", sm: "0" }}
               className="z-10 rounded-bl-[32px] "
             >
               <Heading
@@ -96,7 +94,7 @@ const HeroSection = ({
               >
                 {buttonHeading}
               </Heading>
-              {/* donate links */}
+              {/* links */}
               <Flex
                 direction={{ initial: "column", sm: "row" }}
                 gap={{ initial: "3", sm: "2", lg: "5" }}
@@ -105,7 +103,7 @@ const HeroSection = ({
                 mr={{ initial: "auto", sm: "9" }}
                 ml={{ initial: "auto", sm: "0" }}
               >
-                {buttons.map(({ label, href, invertColors, targetBlank }) => (
+                {buttons.map(({ label, href, bgWhite, targetBlank }) => (
                   <Box
                     asChild
                     width={{ initial: "100%", sm: "auto" }}
@@ -113,9 +111,9 @@ const HeroSection = ({
                   >
                     <Button
                       className={
-                        invertColors
-                          ? "bg-navy-600 hover:bg-navy-500 "
-                          : "bg-white text-black hover:bg-navy-500 hover:text-white transition-200"
+                        bgWhite
+                          ? "bg-white text-navy-700 no-underline hover:bg-navy-50"
+                          : ""
                       }
                       size={{ initial: "3", sm: "2", md: "3" }}
                       asChild
@@ -135,7 +133,7 @@ const HeroSection = ({
           </Flex>
         </Flex>
       </Flex>
-    </>
+    </Box>
   );
 };
 
