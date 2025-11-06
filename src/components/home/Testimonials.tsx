43a50144cf7c7484b4a2a53ea9aa9fa5dd0cc679
Graham Hall <graham@ghall.dev>
2025-11-06 13:52:15 -0500

    apply feedback

--- src/components/home/Testimonials.tsx
+++ src/components/home/Testimonials.tsx
@@ -1,49 +1,66 @@
 import { Box, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Image from "next/image";
 
 import { partnerTestimonials } from "@/data/home";
 
 const Testimonials = () => {
   return (
-    <Box p="7">
-      <Box className="text-center" mb="8">
-        <Heading as="h2" className="text-navy-900 text-6xl" m="4">
+    <Box p="7" aria-labelledby="testimonial-heading">
+      <Flex direction="column" align="center" mb="8">
+        <Heading
+          as="h2"
+          className="text-navy-900 text-6xl"
+          m="4"
+          id="testimonial-heading"
+        >
           Partner Testimonials
         </Heading>
         <Text size="4" m="4">
           Insights from organizations working alongside us.
         </Text>
-      </Box>
+      </Flex>
       <Grid
         columns={{
           initial: "1",
           lg: "2",
         }}
         gap="7"
+        asChild
       >
-        {partnerTestimonials.map(({ name, role, quote, img }) => (
-          <Flex
-            className="bg-slate-50 rounded-2xl border border-slate-200 p-8"
-            key={name}
-            direction="column"
-            gap="5"
-          >
-            <Flex gap="4" align="center">
-              <img
-                className="rounded-lg"
-                src={`/images/home/partner-testimonials/${img.src}`}
-                alt={img.alt}
-                loading="lazy"
-                width={112}
-                height={112}
-              />
-              <Flex direction="column" gap="2">
-                <Text className="text-2xl">{name}</Text>
-                <Text className="text-lg">{role}</Text>
+        <ul>
+          {partnerTestimonials.map(({ name, role, quote, img }, index) => (
+            <li key={`${index}-${name}}`}>
+              <Flex
+                className="bg-slate-50 rounded-2xl border border-slate-200 p-8"
+                direction="column"
+                gap="5"
+                asChild
+              >
+                <article aria-labelledby={`t-${index}-name`}>
+                  <Flex gap="4" align="center">
+                    <Image
+                      className="rounded-lg"
+                      src={`/images/home/partner-testimonials/${img.src}`}
+                      alt={img.alt}
+                      loading="lazy"
+                      width={112}
+                      height={112}
+                    />
+                    <Flex direction="column" gap="2">
+                      <Text className="text-2xl" id={`t-${index}-name`}>
+                        {name}
+                      </Text>
+                      <Text className="text-lg">{role}</Text>
+                    </Flex>
+                  </Flex>
+                  <blockquote className="font-bold italic text-2xl">
+                    "{quote}"
+                  </blockquote>
+                </article>
               </Flex>
-            </Flex>
-            <Text className="font-bold italic text-2xl">"{quote}"</Text>
-          </Flex>
-        ))}
+            </li>
+          ))}
+        </ul>
       </Grid>
     </Box>
   );
