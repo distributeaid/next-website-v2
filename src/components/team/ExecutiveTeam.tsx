cbd95cd2d130ceba225149980228692a7a6ff4b4
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-11-06 10:12:07 -0600

    Team page skeleton

---
+++ src/components/team/ExecutiveTeam.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { Container, Flex, Heading, Section, Text } from "@radix-ui/themes";
+
+export const ExecutiveTeam: FC = () => (
+  <Section>
+    <Heading>Executive Team</Heading>
+  </Section>
+);
