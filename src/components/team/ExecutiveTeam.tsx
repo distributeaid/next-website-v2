acd457bc4e501de52d72ee4cd11d869955cb1886
Alexa Kirchner <akirchner333@users.noreply.github.com>
2025-11-07 15:13:37 -0600

    All of the required elements

--- src/components/team/ExecutiveTeam.tsx
+++ src/components/team/ExecutiveTeam.tsx
@@ -1,12 +1,62 @@
 import { FC } from "react";
 import { Box, Container, Flex, Heading, Section, Text } from "@radix-ui/themes";
+import { ExecutiveCard } from "./ExecutiveCard";
+
+const executives = [
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+  {
+    name: "Sara Lonegard",
+    title: "Executive Director",
+    photo: "/images/tech/Sponsor-DA-Tech.jpg",
+  },
+];
 
 export const ExecutiveTeam: FC = () => (
   <Section>
     <Heading>Executive Team</Heading>
     <Section>
-
+      {executives.map((item, index) => (
+        <ExecutiveCard
+          name={item.name}
+          title={item.title}
+          photo={item.photo}
+          key={`exec-${index}`}
+        />
+      ))}
     </Section>
   </Section>
 );
-
