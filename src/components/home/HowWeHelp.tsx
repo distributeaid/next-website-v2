f85f433741cd57dec8712c2bc825ff4da3de9320
Amoreena Hagedorn <amoreena.hagedorn@lightstep.com>
2025-11-19 11:42:46 -0800

    adjust components to layout matches figma

--- src/components/home/HowWeHelp.tsx
+++ src/components/home/HowWeHelp.tsx
@@ -1,6 +1,7 @@
-import { Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid, Heading, Section, Text } from "@radix-ui/themes";
 import { ReactNode } from "react";
 import { FaRegFileAlt, FaTruck, FaBook } from "react-icons/fa";
+import Image from "next/image";
 
 const informationColumn = [
   "Information",
@@ -43,73 +44,107 @@ interface CircleIconProps {
 }
 
 const CircleIcon = ({ color = "bg-navy-900" }: CircleIconProps) => (
-  <span
-    className={`inline-flex items-center justify-center w-6 h-6 rounded-full ${color}`}
-    aria-hidden="true"
-  ></span>
+  <Box className={`size-6 rounded-full ${color}`} aria-hidden="true" />
+);
+
+const GridHeader = ({ label }: { label: string }) => (
+  <Flex gap="2" align="center">
+    {headerToIconMap[label]}
+    <Text className="text-navy-900" size="5" weight="bold">
+      {label}
+    </Text>
+  </Flex>
 );
 
 const GridItem = ({
-  text,
-  circleIconColor,
+  label,
+  colorIdx,
 }: {
-  text: string;
-  circleIconColor?: number;
+  label: string;
+  colorIdx?: number;
 }) => (
-  <div className="flex items-center gap-2">
-    <CircleIcon color={informationColumnColors[circleIconColor ?? 0]} />
-    <span className="text-navy-900 font-medium">{text}</span>
-  </div>
-);
-const GridHeader = ({ text }: { text: string }) => (
-  <div className="flex items-center gap-2">
-    {headerToIconMap[text]}
-    <span className="text-navy-900 font-large font-semibold">{text}</span>
-  </div>
+  <Flex gap="2" align="center">
+    <CircleIcon color={informationColumnColors[colorIdx ?? 0]} />
+    <Text className="text-navy-900" size="4" weight="medium">
+      {label}
+    </Text>
+  </Flex>
 );
 
-const GridColumn = ({ items }: { items: string[] }) => (
-  <div className="flex flex-col gap-4">
-    {items.map((item, index) => {
-      if (index === 0) {
-        return <GridHeader key={item} text={item} />;
-      }
-      return <GridItem key={item} text={item} circleIconColor={index} />;
-    })}
-  </div>
+const GridColumn = ({ column }: { column: string[] }) => (
+  <Flex direction="column" gap="4" align="start">
+    {column.map((label, idx) =>
+      idx === 0 ? (
+        <GridHeader key={label} label={label} />
+      ) : (
+        <GridItem key={label} label={label} colorIdx={idx} />
+      ),
+    )}
+  </Flex>
 );
+
 const HowWeHelp = () => {
   return (
-    <section className="mb-12" aria-labelledby="how-we-help-heading">
-      {/* Banner */}
-      <h2
-        id="how-we-help-heading"
-        className="bg-[--purple-7] text-navy-900 py-4 px-4 mb-6 text-center text-lg md:text-2xl shadow"
+    <Box className="bg-slate-100">
+      <Box
+        className="bg-[--purple-7] text-navy-900 shadow"
+        p="4"
+        mb="8"
+        asChild
       >
-        How we help get <span className="font-semibold">the right</span> aid
-        delivered
-      </h2>
-      {/* Radix UI Grid */}
-      <Grid
-        columns="3"
-        gap="4"
-        rows="repeat(2, 64px)"
-        width="auto"
-        className="px-4"
-        role="list"
-      >
-        <div className="col-span-3 w-full flex justify-center">
-          <img
-            src="images/home/da-circular-economy.png"
+        <Heading
+          as="h2"
+          id="how-we-help-heading"
+          align="center"
+          size={{
+            initial: "5",
+            md: "7",
+          }}
+        >
+          How we help get{" "}
+          <Text weight="bold" as="span">
+            the right
+          </Text>{" "}
+          aid delivered
+        </Heading>
+      </Box>
+      {/* Content Section */}
+      <Section p="9" pt="0">
+        {/* Image Section */}
+        <Flex justify="center" mb="8">
+          <Image
+            src="/images/home/da-circular-economy.png"
             alt="Flow of donations: to hub, then to grassroots aid."
-            className="w-full max-w-[800px] min-h-[600px] h-auto object-cover rounded-lg"
+            width={1000}
+            height={600}
+            className="object-cover rounded-lg"
+            loading="lazy"
           />
-        </div>
-        {items.map((column, idx) => (
-          <GridColumn key={idx} items={column} />
-        ))}
-      </Grid>
-    </section>
+        </Flex>
+        {/* Grid Section */}
+        <Flex justify="center" width="100%">
+          <Grid
+            columns={{
+              initial: "1",
+              lg: "3",
+            }}
+            width="100%"
+            gap="8"
+            asChild
+          >
+            <ul aria-label="How We Help Columns">
+              {items.map((column, idx) => (
+                <li key={idx} aria-label={`How We Help Column ${idx + 1}`}>
+                  <Flex justify="center" width="100%">
+                    <GridColumn column={column} />
+                  </Flex>
+                </li>
+              ))}
+            </ul>
+          </Grid>
+        </Flex>
+      </Section>
+    </Box>
   );
 };
 
