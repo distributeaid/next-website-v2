a28990181504ad4de162f67dddc5653b86a2ef39
Graham Hall <graham@ghall.dev>
2025-11-19 12:24:00 -0500

    refactor

---
+++ src/components/home/icons/DonateSupplies.tsx
@@ -0,0 +1,47 @@
+export default function DonateSupplies() {
+  return (
+    <svg
+      width="104"
+      height="104"
+      viewBox="0 0 104 104"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M69.3335 69.3332L78.0002 77.9998L95.3335 60.6665"
+        stroke="#082B76"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M91 43.3336V34.6669C90.9984 33.1471 90.5973 31.6544 89.8367 30.3386C89.0761 29.0228 87.9829 27.9301 86.6667 27.1702L56.3333 9.83689C55.0158 9.07624 53.5213 8.67578 52 8.67578C50.4787 8.67578 48.9842 9.07624 47.6667 9.83689L17.3333 27.1702C16.0171 27.9301 14.9239 29.0228 14.1633 30.3386C13.4028 31.6544 13.0016 33.1471 13 34.6669V69.3336C13.0016 70.8534 13.4028 72.3461 14.1633 73.6619C14.9239 74.9777 16.0171 76.0703 17.3333 76.8302L47.6667 94.1636C48.9842 94.9242 50.4787 95.3247 52 95.3247C53.5213 95.3247 55.0158 94.9242 56.3333 94.1636L65 89.2236"
+        stroke="#082B76"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M71.4999 40.733L32.7166 18.373"
+        stroke="#082B76"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14.2566 30.3335L51.9999 52.0002L89.7432 30.3335"
+        stroke="#082B76"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M52 95.3338V52.0005"
+        stroke="#082B76"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
