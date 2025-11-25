0b41a26e04da2480364596b0ff2430221615d692
Michael <micaheledquilan@gmail.com>
2025-11-25 13:11:15 -0500

    imported all relevant sections in contact page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,29 +1,18 @@
 import ContactForm from "@/components/contact-us/ContactForm";
-import GotAidSection from "@/components/contact-us/GotAidSection";
-import ImageHeader from "@/components/contact-us/ImageHeader";
+// import GotAidSection from "@/components/contact-us/GotAidSection";
+// import ImageHeader from "@/components/contact-us/ImageHeader";
 import SupportUsSection from "@/components/contact-us/SupportUsSection";
-import VolunteerSection from "@/components/contact-us/VolunteerSection";
-import { Section } from "@radix-ui/themes";
+// import VolunteerSection from "@/components/contact-us/VolunteerSection";
+import { Container } from "@radix-ui/themes";
+import { ContactPageTitle } from "@/components/contact-us/ContactPageTitle";
 const page = () => {
   return (
     <>
-      {/* example use of ImageHeader */}
-      {/* <ImageHeader
-        imageUrl="/images/contact-us/gift-graphic.png"
-        heading="Support Us"
-      /> */}
-      <Section>
+      <ContactPageTitle />
+      <Container>
         <ContactForm />
-      </Section>
-      <Section>
-        <GotAidSection />
-      </Section>
-      <Section>
         <SupportUsSection />
-      </Section>
-      <Section>
-        <VolunteerSection />
-      </Section>
+      </Container>
     </>
   );
 };
