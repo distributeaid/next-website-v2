b28e0227541786b4ed66cb073a972e02c6723d6d
Michael <micaheledquilan@gmail.com>
2025-11-25 13:12:20 -0500

    removed unused components

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,8 +1,5 @@
 import ContactForm from "@/components/contact-us/ContactForm";
-// import GotAidSection from "@/components/contact-us/GotAidSection";
-// import ImageHeader from "@/components/contact-us/ImageHeader";
 import SupportUsSection from "@/components/contact-us/SupportUsSection";
-// import VolunteerSection from "@/components/contact-us/VolunteerSection";
 import { Container } from "@radix-ui/themes";
 import { ContactPageTitle } from "@/components/contact-us/ContactPageTitle";
 const page = () => {
