67d8f8c9a0179efa30cb6f06f3895c3db29e5aab
yenterefe <yenterefe@example.com>
2025-11-27 09:30:47 -0500

    fix: color hydration and separation of data and UI

---
+++ src/components/team/BoardMembers.tsx
@@ -0,0 +1,42 @@
+export const boardMembers = [
+  {
+    id: "sara-lonegard-1",
+    name: "Sara Lonegard",
+    source: "/images/checkerboard.svg",
+    alt: "board member photo",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laudantium, error consequuntur sequi ipsum cumque ratione dolores assumenda facilis soluta.",
+  },
+  {
+    id: "sara-lonegard-2",
+    name: "Sara Lonegard",
+    source: "/images/checkerboard.svg",
+    alt: "board member photo",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laudantium, error consequuntur sequi ipsum cumque ratione dolores assumenda facilis soluta.",
+  },
+  {
+    id: "sara-lonegard-3",
+    name: "Sara Lonegard",
+    source: "/images/checkerboard.svg",
+    alt: "board member photo",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laudantium, error consequuntur sequi ipsum cumque ratione dolores assumenda facilis soluta.",
+  },
+  {
+    id: "sara-lonegard-4",
+    name: "Sara Lonegard",
+    source: "/images/checkerboard.svg",
+    alt: "board member photo",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laudantium, error consequuntur sequi ipsum cumque ratione dolores assumenda facilis soluta.",
+  },
+  {
+    id: "sara-lonegard-5",
+    name: "Sara Lonegard",
+    source: "/images/checkerboard.svg",
+    alt: "board member photo",
+    blurb:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum laudantium, error consequuntur sequi ipsum cumque ratione dolores assumenda facilis soluta.",
+  },
+];
