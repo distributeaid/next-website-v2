fd8989f16b7ccb6de488e0e112fa33ec9f99c05e
Michael <micaheledquilan@gmail.com>
2025-12-10 13:01:33 -0500

    added extra margin around full container rather than only top

--- src/components/team/IntroSection.tsx
+++ src/components/team/IntroSection.tsx
@@ -4,7 +4,7 @@ const IntroSection = () => {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue  tempus aliquam. Suspendisse molestie nisl sit amet dui cursus placerat.  Sed lacinia pulvinar ligula, non pellentesque risus dapibus eget. Proin  porta efficitur risus. Aliquam eget maximus elit, nec sodales dui.  Curabitur venenatis congue arcu eget luctus. Vestibulum ante ipsum  primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean et sollicitudin tortor, ac auctor ligula. Mauris eget nunc vestibulum,  ullamcorper libero vitae, cursus turpis. Etiam vulputate libero vitae  diam blandit, id porta nisi consequat. Maecenas sed lobortis quam.  Pellentesque lobortis erat eros, pellentesque feugiat mi sodales et.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum et  malesuada fames ac ante ipsum primis in faucibus. Nunc id erat  efficitur, facilisis est vitae, consequat mi. Donec at leo nibh.";
   return (
     <>
-      <Container mt="3">
+      <Container m={"3"}>
         <Flex align={"center"} gapY={"9"} direction={"column"}>
           <Heading
             className="underline decoration-2 underline-offset-[30px] uppercase"
