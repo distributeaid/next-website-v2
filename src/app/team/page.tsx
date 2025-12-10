4fb519ec6f88a0f9462d0cde5030caf0f1afe6f3
Michael <micaheledquilan@gmail.com>
2025-12-10 12:06:46 -0500

    feat: created intro section for team page. Just need the intro text copy to be finalized

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,9 +1,11 @@
 import React, { FC } from "react";
 import { ExecutiveTeam } from "@/components/team/ExecutiveTeam";
+import IntroSection from "@/components/team/IntroSection";
 
 const Team: FC = () => {
   return (
     <main>
+      <IntroSection />
       <ExecutiveTeam />
     </main>
   );
---
+++ src/components/team/IntroSection.tsx
@@ -0,0 +1,31 @@
+import { Flex, Heading, Text, Container } from "@radix-ui/themes";
+const IntroSection = () => {
+  const introText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue  tempus aliquam. Suspendisse molestie nisl sit amet dui cursus placerat.  Sed lacinia pulvinar ligula, non pellentesque risus dapibus eget. Proin  porta efficitur risus. Aliquam eget maximus elit, nec sodales dui.  Curabitur venenatis congue arcu eget luctus. Vestibulum ante ipsum  primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean et sollicitudin tortor, ac auctor ligula. Mauris eget nunc vestibulum,  ullamcorper libero vitae, cursus turpis. Etiam vulputate libero vitae  diam blandit, id porta nisi consequat. Maecenas sed lobortis quam.  Pellentesque lobortis erat eros, pellentesque feugiat mi sodales et.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum et  malesuada fames ac ante ipsum primis in faucibus. Nunc id erat  efficitur, facilisis est vitae, consequat mi. Donec at leo nibh.";
+  return (
+    <>
+      <Container mt="3">
+        <Flex align={"center"} gapY={"9"} direction={"column"}>
+          <Heading
+            className="underline decoration-2 underline-offset-[30px] uppercase"
+            size={{ initial: "8", sm: "9" }}
+            style={{ color: "var(--blue-header)" }}
+            as="h1"
+            align={"center"}
+          >
+            Distribute Aid Team
+          </Heading>
+          <Text
+            weight={"bold"}
+            style={{ color: "var(--blue-background)" }}
+            align={"center"}
+            as="p"
+          >
+            {introText}
+          </Text>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+export default IntroSection;
