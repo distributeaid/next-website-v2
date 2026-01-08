aa12fbb40835c09c66c9eab3dc2522e5320df5bb
Michael <micaheledquilan@gmail.com>
2026-01-08 13:34:29 -0500

    tech-debt: combined donate hero section with home hero section in /components/ui. Added a parameter for the buttons bg color and optional paramter for showing DA logo, defaulted true.

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -1,14 +1,24 @@
 import DonateGrid from "@/components/donate/DonateGrid";
 import FUND_RAISERS from "@/data/donate.json";
-import HeroSection from "@/components/donate/HeroSection";
+// import HeroSection from "@/components/donate/HeroSection";
 import CopySection from "@/components/donate/CopySection";
 import DonateLandingText from "@/components/donate/DonateLandingText";
 import WingOfHonor from "@/components/donate/WingOfHonor";
+import HeroSection from "@/components/ui/HeroSection";
 
 const Page = () => {
   return (
     <>
-      <HeroSection />
+      {/* <HeroSection /> */}
+      <HeroSection
+        heading="Support us today, so communities are prepared for
+              tomorrow."
+        imgSrc="/images/donate/european-refugee-relief-hero-tinted.png"
+        imgAlt="european-refugee-relief-hero-tinted"
+        buttonHeading="Ship aid to people in need."
+        buttonBGColor="var(--green-9)"
+        hasLogo={false}
+      />
       <CopySection />
       {FUND_RAISERS && FUND_RAISERS.length > 0 && (
         <DonateGrid funds={FUND_RAISERS} />
