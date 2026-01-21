4989e4ce909b4ab9a760364326d1c1bf6a60b1a6
Graham Hall <graham@ghall.dev>
2026-01-21 09:19:23 -0500

    security headers

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,51 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  poweredByHeader: false,
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `
+             default-src 'self';
+             script-src 'self';
+             style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+             font-src 'self' https://fonts.gstatic.com;
+             img-src 'self' data:;
+             object-src 'none';
+             base-uri 'self';
+             frame-ancestors 'none';
+             form-action 'self'
+           `
+              .replace(/\s+/g, " ")
+              .trim(),
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), geolocation=(), microphone=(), payment=(), accelerometer=(), gyroscope=()",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
