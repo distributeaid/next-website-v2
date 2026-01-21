a6f3103702bcc767594515377f259312e2a8054f
Alexa Hyde <alexa@hyde.com>
2026-01-21 15:09:09 -0600

    Prettier

--- src/utils/strapi/api.ts
+++ src/utils/strapi/api.ts
@@ -1,19 +1,19 @@
-"use server"
-import { TeamMember } from "./types"
+"use server";
+import { TeamMember } from "./types";
 
-async function strapiGet(url: string){
-	const { STRAPI_URL, STRAPI_KEY } = process.env;
+async function strapiGet(url: string) {
+  const { STRAPI_URL, STRAPI_KEY } = process.env;
   return await fetch(`${STRAPI_URL}/api/${url}`, {
-    cache: 'force-cache',
+    cache: "force-cache",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${STRAPI_KEY}`
-    }
+      Authorization: `Bearer ${STRAPI_KEY}`,
+    },
   });
 }
 
-export async function getTeam(){
-	const { STRAPI_URL } = process.env;
+export async function getTeam() {
+  const { STRAPI_URL } = process.env;
   const response = await strapiGet("members?populate=*");
   const jsonData = await response.json();
   return jsonData.data.map((d: TeamMember) => ({
