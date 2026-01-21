4b669548f643fc8c0787c4155f08926e35cb4ed9
Graham Hall <graham@ghall.dev>
2026-01-21 15:37:08 -0500

    allow donor badges to be embedded on external page

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,17 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: "/donor-recognition/:path*.{jpg,jpeg,png,gif,webp}",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Accept, Origin",
+          },
+        ],
+      },
     ];
   },
 };
