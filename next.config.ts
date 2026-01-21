4e6eb46396910714426587d986827b92fb009495
Graham Hall <graham@ghall.dev>
2026-01-21 14:16:23 -0500

    fix?

--- next.config.ts
+++ next.config.ts
@@ -16,12 +16,12 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-             default-src 'self';
-             script-src 'self' 'unsafe-inline' 'unsafe-eval';
-             connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
-             img-src 'self' data: image/png image/jpg;
-             font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com;
-             style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              connect-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+              img-src 'self' data: image/png image/jpg;
+              font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
            `,
           },
           {
