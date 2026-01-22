46d7c06163fcea741ca0cab34086a4483efdf33c
Graham Hall <graham@ghall.dev>
2026-01-22 13:52:23 -0500

    clean up eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["standard-with-typescript", "plugin:react/recommended"],
+  extends: ["next/core-web-vitals"],
   overrides: [
     {
       env: {
@@ -19,13 +19,9 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react"],
   rules: {
     // disable the rule for all files
     "@typescript-eslint/explicit-function-return-type": "off",
-  },
-  extends: "next",
-  rules: {
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off",
   },
