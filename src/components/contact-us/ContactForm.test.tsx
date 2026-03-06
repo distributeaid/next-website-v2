c9a9963c181351c9330ce2e26e4a46d2a8c28222
Corey Hindersinn <chindersinn@gmail.com>
2026-03-06 10:55:23 -0800

    Initial test draft

---
+++ src/components/contact-us/ContactForm.test.tsx
@@ -0,0 +1,230 @@
+import "@testing-library/jest-dom/vitest";
+import { afterEach, expect, it, describe, vi } from "vitest";
+import { render, screen, cleanup } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ContactForm from "./ContactForm";
+
+const setup = () => {
+  const fetchSpy = vi.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(),
+    }),
+  );
+  global.fetch = fetchSpy;
+
+  render(<ContactForm />);
+
+  const user = userEvent.setup();
+  const firstNameInput = screen.getByRole("textbox", {
+    name: "First Name",
+  }) as HTMLInputElement;
+  const lastNameInput = screen.getByRole("textbox", {
+    name: "Last Name",
+  }) as HTMLInputElement;
+  const emailInput = screen.getByRole("textbox", {
+    name: "Email Address",
+  }) as HTMLInputElement;
+  const messageInput = screen.getByRole("textbox", {
+    name: "Your Message",
+  }) as HTMLInputElement;
+  const submitButton = screen.getByRole("button", { name: "Send message" });
+
+  const setAllValidInputs = async () => {
+    await user.type(firstNameInput, "Jane");
+    await user.type(lastNameInput, "Doe");
+    await user.type(emailInput, "jane.doe@gmail.com");
+    await user.type(messageInput, "My message here");
+  };
+
+  const mockResolvedStatus = (status: number) => {
+    fetchSpy.mockResolvedValueOnce(new Response(null, { status }));
+  };
+
+  return {
+    fetchSpy,
+    user,
+    firstNameInput,
+    lastNameInput,
+    emailInput,
+    messageInput,
+    submitButton,
+    mockResolvedStatus,
+    setAllValidInputs,
+  };
+};
+
+afterEach(() => {
+  vi.clearAllMocks(); // Reset all mocked calls between tests
+  cleanup();
+});
+
+// describe("when no input in required field", () => {
+//   describe("first name", () => {
+//     // it("displays a field error", async () => {
+//     //   const { user, submitButton, firstNameInput } = setup();
+
+//     //   await user.click(submitButton);
+
+//     //   console.log(firstNameInput.validationMessage);
+//     //   console.log(firstNameInput.checkValidity());
+//     //   console.log(firstNameInput.validity);
+//     //   const error = screen.getByText(/Please fill out this field/i);
+//     //   expect(error).toBeInTheDocument();
+//     // });
+
+//     it("makes no API call", async () => {
+//       const { user, submitButton, setAllValidInputs, firstNameInput } = setup();
+//       await setAllValidInputs();
+
+//       await user.clear(firstNameInput);
+//       await user.click(submitButton);
+
+//       expect(fetch).not.toHaveBeenCalled();
+//     });
+//   });
+
+//   // TODO: add more fields here
+// });
+
+describe("does not call post API when required field", () => {
+  it("first name is empty", async () => {
+    const { user, submitButton, setAllValidInputs, firstNameInput } = setup();
+    await setAllValidInputs();
+
+    await user.clear(firstNameInput);
+    await user.click(submitButton);
+
+    expect(fetch).not.toHaveBeenCalled();
+  });
+
+  it("last name is empty", async () => {
+    const { user, submitButton, setAllValidInputs, lastNameInput } = setup();
+    await setAllValidInputs();
+
+    await user.clear(lastNameInput);
+    await user.click(submitButton);
+
+    expect(fetch).not.toHaveBeenCalled();
+  });
+
+  it("message is empty", async () => {
+    const { user, submitButton, setAllValidInputs, messageInput } = setup();
+    await setAllValidInputs();
+
+    await user.clear(messageInput);
+    await user.click(submitButton);
+
+    expect(fetch).not.toHaveBeenCalled();
+  });
+
+  it("email is empty", async () => {
+    const { user, submitButton, setAllValidInputs, emailInput } = setup();
+    await setAllValidInputs();
+
+    await user.clear(emailInput);
+    await user.click(submitButton);
+
+    expect(fetch).not.toHaveBeenCalled();
+  });
+
+  it("email is malformed", async () => {
+    const { user, submitButton, setAllValidInputs, emailInput } = setup();
+    await setAllValidInputs();
+
+    await user.clear(emailInput);
+    await user.type(emailInput, "bademail@");
+    await user.click(submitButton);
+
+    expect(fetch).not.toHaveBeenCalled();
+  });
+});
+
+describe("when all fields are populated correctly", () => {
+  it("sends form data to API", async () => {
+    const {
+      user,
+      firstNameInput,
+      lastNameInput,
+      messageInput,
+      emailInput,
+      submitButton,
+      fetchSpy,
+    } = setup();
+    const firstName = "Maria";
+    const lastName = "Hill";
+    const email = "maria.hill@gmail.com";
+    const message = "Maria's message here";
+
+    await user.type(firstNameInput, firstName);
+    await user.type(lastNameInput, lastName);
+    await user.type(emailInput, email);
+    await user.type(messageInput, message);
+    await user.click(submitButton);
+
+    expect(fetchSpy).toHaveBeenCalledOnce();
+    const [path, contents] = fetchSpy.mock.calls[0];
+    expect(path).toEqual("/api/send");
+    expect(contents).toEqual({
+      body: `{"firstName":"${firstName}","lastName":"${lastName}","email":"${email}","message":"${message}"}`,
+      method: "POST",
+    });
+  });
+});
+
+describe("on API success", () => {
+  it("shows success text", async () => {
+    const { user, submitButton, mockResolvedStatus, setAllValidInputs } =
+      setup();
+    await setAllValidInputs();
+    mockResolvedStatus(200);
+
+    await user.click(submitButton);
+
+    expect(await screen.getByTestId("success")).toBeVisible();
+  });
+
+  it("hides the form", async () => {
+    const { user, submitButton, mockResolvedStatus, setAllValidInputs } =
+      setup();
+    await setAllValidInputs();
+    mockResolvedStatus(200);
+
+    await user.click(submitButton);
+
+    expect(await screen.queryByTestId("form")).toBeNull();
+  });
+
+  it("does not show error dialog", async () => {
+    const { user, submitButton, mockResolvedStatus, setAllValidInputs } =
+      setup();
+    await setAllValidInputs();
+    mockResolvedStatus(200);
+
+    await user.click(submitButton);
+
+    expect(await screen.queryByTestId("error")).toBeNull();
+  });
+});
+
+describe("on API error", () => {
+  it("with error response: shows error dialog", async () => {
+    const { user, submitButton, mockResolvedStatus, setAllValidInputs } =
+      setup();
+    await setAllValidInputs();
+    mockResolvedStatus(404);
+
+    await user.click(submitButton);
+
+    expect(await screen.getByTestId("error")).toBeVisible();
+  });
+
+  it("with promise rejection: shows error dialog", async () => {
+    const { fetchSpy, user, submitButton, setAllValidInputs } = setup();
+    await setAllValidInputs();
+    fetchSpy.mockRejectedValueOnce(new Error());
+
+    await user.click(submitButton);
+
+    expect(await screen.getByTestId("error")).toBeVisible();
+  });
+});
