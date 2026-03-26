35eccdd7aa43954419e13a508b6749d6460d4be9
Graham Hall <graham@ghall.dev>
2026-03-26 15:09:27 -0400

    enhancement/1025/accessible-social-media-icons (#1060)

--- src/components/nav/navBar.tsx
+++ src/components/nav/navBar.tsx
@@ -52,7 +52,10 @@ const NavBar = () => {
                 gapX={"2"}
               >
                 {SOCIAL_LINKS.map((social) => (
-                  <NavigationMenu.Item key={social.name}>
+                  <NavigationMenu.Item
+                    key={social.name}
+                    aria-label={social.ariaLabel}
+                  >
                     <NavigationMenu.Link href={social.link} target="_blank">
                       <Image
                         src={`/images/icons/icon-social-${social.name}.svg`}
