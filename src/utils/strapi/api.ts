d2b58c41132d7ee94a05bb2249b518c39ddc4c4e
Graham Hall <graham@ghall.dev>
2026-04-09 15:02:18 -0400

    fix/noissue/strapi-fix (#1083)

--- src/utils/strapi/api.ts
+++ src/utils/strapi/api.ts
@@ -4,20 +4,43 @@ import { TeamMember } from "./types";
 // The universal get function for the strapi API.
 // The path you use here depends on the data you're looking for
 // as defined in https://github.com/distributeaid/aggregated-public-information
-async function strapiGet(urlPath: string): Promise<any> {
+async function strapiGet(
+  urlPath: string,
+  query?: { [key: string]: any },
+): Promise<any> {
   const { STRAPI_URL, STRAPI_KEY } = process.env;
-  return await fetch(`${STRAPI_URL}/api/${urlPath}`, {
-    cache: "force-cache",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${STRAPI_KEY}`,
-    },
+
+  if (!STRAPI_URL) {
+    throw Error("Missing STRAPI_URL");
+  }
+
+  if (!STRAPI_KEY) {
+    throw Error("Missing STRAPI_KEY");
+  }
+
+  const url = new URL(`${STRAPI_URL}`);
+  url.pathname = `/api/${urlPath}`;
+
+  if (query) {
+    for (let [key, value] of Object.entries(query)) {
+      url.searchParams.append(key, value);
+    }
+  }
+
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${STRAPI_KEY}`,
+  };
+
+  return await fetch(url, {
+    cache: "no-cache",
+    headers,
   });
 }
 
 // Pulls a list of team members from the strapi API
 export async function getTeam(): Promise<TeamMember[]> {
-  const response = await strapiGet("members?populate=*");
+  const response = await strapiGet("members", { populate: "*" });
   const jsonData = await response.json();
   return jsonData.data;
 }
