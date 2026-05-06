f0080a70a5fbe521226561a34d983c830d3be698
Michael Edquilan <54874700+littilemike11@users.noreply.github.com>
2026-05-06 14:47:00 -0400

    fix/added response page titles to messages.ts for resusablity

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -20,3 +20,13 @@ export const TECH_PAGE_TITLE = "Tech";
 export const DONATE_PAGE_TITLE = "Donate";
 
 export const RESPONSES_PAGE_TITLE = "Responses";
+
+export const US_DISASTER_PREPAREDNESS_RESPONSE_PAGE_TITLE =
+  "US Disaster Preparedness";
+
+export const LEVANT_RESPONSE_PAGE_TITLE = "Levant Response";
+
+export const REFUGEE_SUPPORT_EUROPE_RESPONSE_PAGE_TITLE =
+  "Refugee Support Europe";
+
+export const HRT_TOOLKIT_RESPONSE_PAGE_TITLE = "HRT Harm Reduction Toolkit";
